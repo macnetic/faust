550f23f71178089bab77bb6dc019146ff87246b9
Thomas Gautrais <thomas.gautrais@inria.fr>
2015-06-10 12:49:05 +0000

    modif dans trunk/devcpp/init_from_matlab

--- init_from_matlab/faust_init_from_matio.cpp
+++ init_from_matlab/faust_init_from_matio.cpp
@@ -33,7 +33,7 @@ void init_faust_mat_from_matio_mat(faust_mat& M, const char* fileName, const cha
    if( matvar->class_type != MAT_C_DOUBLE
        || matvar->rank != 2)
    {
-      cerr << "error in init_faust_mat_from_matio_mat : "<< variableName << "seems not to be a matrix." << endl;
+      cerr << "error in init_faust_mat_from_matio_mat : "<< variableName << " seems not to be a matrix." << endl;
       exit(EXIT_FAILURE);
    }
    M.resize(matvar->dims[0], matvar->dims[1]);
@@ -46,12 +46,14 @@ double init_faust_mat_from_matio_double(const char* fileName, const char* variab
 {
    matvar_t* matvar = faust_matio_read_variable(fileName, variableName);
 
+cout<<variableName<<".class_type="<<matvar->class_type<<endl;
+
    if( matvar->class_type != MAT_C_DOUBLE
        || matvar->rank != 2
        || matvar->dims[0] != 1
        || matvar->dims[1] != 1)
    {
-      cerr << "error in init_faust_mat_from_matio_double : "<< variableName << "seems not to be a scalar." << endl;
+      cerr << "error in init_faust_mat_from_matio_double : "<< variableName << " seems not to be a scalar." << endl;
       exit(EXIT_FAILURE);
    }
    double val = ((double*)(matvar->data))[0];
@@ -63,9 +65,9 @@ int init_faust_mat_from_matio_int(const char* fileName, const char* variableName
 {
    double val_d = init_faust_mat_from_matio_double(fileName, variableName);
    int val_i = (int) val_d;
-   if ((double)val_i == val_d)
+   if ((double)val_i != val_d)
      {
-	cerr<<"error in init_faust_mat_from_matio_int :"<< variableName << "seems not to be an integer." <<endl;
+	cerr<<"error in init_faust_mat_from_matio_int : "<< variableName << " seems not to be an integer." <<endl;
 	exit(EXIT_FAILURE);
      }
    return val_i;
@@ -75,14 +77,37 @@ int init_faust_mat_from_matio_int(const char* fileName, const char* variableName
 bool init_faust_mat_from_matio_bool(const char* fileName, const char* variableName)
 {
 
-   int val_i = init_faust_mat_from_matio_int(fileName, variableName);
-   if (val_i==0) 
+   matvar_t* matvar = faust_matio_read_variable(fileName, variableName);
+
+
+   if( (matvar->class_type != MAT_C_DOUBLE && matvar->class_type != MAT_C_UINT8)
+       || matvar->rank != 2
+       || matvar->dims[0] != 1
+       || matvar->dims[1] != 1)
+   {
+      cerr << "error in init_faust_mat_from_matio_bool : "<< variableName << " seems not to be a scalar." << endl;
+      exit(EXIT_FAILURE);
+   }
+   double val;
+   if (matvar->data_type == MAT_T_UINT8)
+      val = ((uint8_t*)(matvar->data))[0];
+   else if (matvar->data_type == MAT_T_DOUBLE)
+      val = ((double*)(matvar->data))[0];
+   else
+   {
+      cerr << "error in init_faust_mat_from_matio_bool : "<< variableName << " wrong data type." << endl;
+      exit(EXIT_FAILURE);
+   }
+   Mat_VarFree(matvar);
+
+
+   if (val==0.0) 
       return false;
-   else if (val_i == 1)
+   else if (val == 1)
       return true;
    else
    {
-      cerr<<"error in init_faust_mat_from_matio_bool :"<< variableName << "seems not to be a boolean." <<endl;
+      cerr<<"error in init_faust_mat_from_matio_bool : "<< variableName << " seems not to be a boolean." <<endl;
       exit(EXIT_FAILURE);
    }
 }
--- init_from_matlab/test/test.cpp
+++ init_from_matlab/test/test.cpp
@@ -1,13 +1,14 @@
 #include "faust_init_from_matio.h"
 #include <iostream>
 #include "faust_constant.h"
+#include "faust_mat.h"
 
 using namespace std;
 
 
 int main()
 {
-   faust_mat A;
+   faust_mat A(10,10);
    faust_real B,E,G;
    int C;
    bool D,F;
@@ -17,10 +18,23 @@ int main()
    C = init_faust_mat_from_matio_int("test.mat", "C");
    D = init_faust_mat_from_matio_bool("test.mat", "D");
    F = init_faust_mat_from_matio_bool("test.mat", "F");
-   G = init_faust_mat_from_matio_double("test.mat", "G");
+   //G = init_faust_mat_from_matio_double("test.mat", "G");
 
 
+  cout<<"A="<<endl;
+  for (int i=0 ; i<A.getNbRow() ; i++)
+  { 
+    for (int j=0 ; j<A.getNbCol() ;j++)
+     {
+        cout << A.getData()[j*A.getNbRow()+i]<<" ";
+     }
+     cout<<endl;
+  }
 
+  cout << endl << "B="<<B<<endl;
+  cout << endl << "C="<<C<<endl;
+  cout << endl << "D="<<D<<endl;
+  cout << endl << "F="<<F<<endl;
    
    
 
