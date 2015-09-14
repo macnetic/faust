55b4c38039b0109d2a76e1f595cd17164dd968c9
Thomas Gautrais <thomas.gautrais@inria.fr>
2015-09-14 16:17:53 +0000

    adding script for windows to find matlabroot

--- CMake/find_matlab_root.bat
+++ CMake/find_matlab_root.bat
@@ -1,7 +1,8 @@
 @echo off
-
-for /f "delims=" %%i in ('where matlab.exe') do set MATLAB_DIR_TMP=%%i
-
+set "find_exe=matlab.exe"
+set MATLAB_DIR_TMP=
+(for /f "delims=" %%i in ('where %find_exe%') do set MATLAB_DIR_TMP=%%i)>NUL 2>&1 
+rem echo %MATLAB_DIR_TMP%
 setlocal enableDelayedExpansion
 set ^"str=%MATLAB_DIR_TMP%"
 for /F "delims=" %%a in (^"!str:\bin^=^
@@ -12,4 +13,6 @@ goto :break
 )
 :break
 
+rem echo "MATLAB" %MATLAB_ROOT_DIR%
 echo|set /p=%MATLAB_ROOT_DIR%
+@echo on
