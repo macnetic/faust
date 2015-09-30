f5fd07da51351d64d06c4e5d2c5505794b4889fc
Thomas Gautrais <thomas.gautrais@inria.fr>
2015-09-30 16:07:34 +0000

    modifying MEX_EXT and MEX_SUBDIR_LIB in the main CMakeLists.txt

--- CMake/matlab_arch.bat
+++ CMake/matlab_arch.bat
@@ -1,9 +1,10 @@
 @echo off
-if "%1"=="" goto end
-if "%2"=="" goto end
+if "%~1"=="" goto end
+if "%~2"=="" goto end
 
 if "%1"=="0" goto matlab_lib_subdir
 if "%1"=="1" goto matlab_ext
+if "%1"=="2" goto matlab_lib_subdir2
 
 :matlab_lib_subdir
 matlab -wait -noFigureWindows -nosplash -r """fid=fopen('%2','w');fprintf(fid,'%%s',computer('arch'));fclose(fid);exit"""
@@ -14,6 +15,11 @@ goto end
 matlab -wait -noFigureWindows -nosplash -r """fid=fopen('%2','w');fprintf(fid,'%%s',mexext);fclose(fid);exit"""
 more %2
 goto end 
+
+:matlab_lib_subdir2
+dir /B "%~2\extern\lib" | findstr /R /C:"^win[16|32|64]"
+goto end 
+
 
 :end
 
