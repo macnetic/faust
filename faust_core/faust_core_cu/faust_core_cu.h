e726910d275798a49775581c36875052c4bbd4d6
Thomas Gautrais <thomas.gautrais@inria.fr>
2016-02-15 11:45:13 +0000

    adding faust_core_cu for gpu

---
+++ faust_core/faust_core_cu/faust_core_cu.h
@@ -0,0 +1,85 @@
+#ifndef __FAUST_CORE_CU_H__
+#define __FAUST_CORE_CU_H__
+
+#include <vector>
+#include "faust_cu_spmat.h"
+
+// class faust_cu_mat<T>;
+// class faust_cu_vec;
+
+
+template<typename T> class faust_cu_mat;
+template<typename T> class faust_cu_spmat;
+template<typename T> class faust_cu_vec;
+template<typename T> class faust_core;
+
+template<typename T>
+faust_cu_vec<T> operator*(const faust_core<T>& f, const faust_cu_vec<T>& v);
+template<typename T>
+faust_cu_mat<T> operator*(const faust_core<T>& f, const faust_cu_mat<T>& M);
+
+
+
+template<typename T>
+class faust_core
+{
+	public:
+		faust_core();
+		faust_core(const std::vector<faust_cu_spmat<T> >& facts, const T lambda_ = (T)1.0);
+		faust_core(const faust_core<T> & A);
+		faust_core(const std::vector<faust_cu_mat<T> >&facts);
+		void get_facts(std::vector<faust_cu_spmat<T> >& sparse_facts)const{sparse_facts = data;}
+		void get_facts(std::vector<faust_cu_mat<T> >& facts)const;	
+		int size()const{return data.size();} 
+                faust_cu_mat<T> get_product()const;
+		faust_cu_spmat<T> get_fact(int id) const;		
+		int getNbRow() const;
+		int getNbCol() const;
+		void print_file(const char* filename) const;
+		void init_from_file(const char* filename);
+		long long int get_total_nnz()const{return totalNonZeros;}
+		void clear(){data.resize(0);totalNonZeros=0;}
+		void push_back(const faust_cu_spmat<T>& S);
+		void push_first(const faust_cu_spmat<T>& S);
+		void pop_back(faust_cu_spmat<T>& S);
+		void pop_first(faust_cu_spmat<T>& S);
+		void pop_first(faust_cu_spmat<T>& S) const;
+		void Display()const;
+		void transpose();
+		void updateNonZeros();
+		//(*this) = (*this) * A
+		void multiply(const faust_core<T> & A);
+		//(*this) = A * (*this)
+		void multiplyLeft(const faust_core<T> & A);	
+		void scalarMultiply(const T scalar);
+		T spectralNorm(const int nbr_iter_max, T threshold, int &flag) const;
+		~faust_core(){}
+
+
+	public:
+		void operator=(const faust_core<T>&  f){data=f.data;totalNonZeros=f.totalNonZeros;}
+		// add all of the sparse matrices from f.data to this->data
+		void operator*=(const T  scalar){scalarMultiply(scalar);};
+		void operator*=(const faust_core<T>&  f){multiply(f);};
+		// add the sparse matrix S to this->data
+		void operator*=(const faust_cu_spmat<T>&  S){push_back(S);totalNonZeros+=S.getNonZeros();}
+
+
+
+
+	private:
+		std::vector<faust_cu_spmat<T> > data;
+		long long int totalNonZeros;
+		static const char * class_name;
+		
+
+	friend faust_cu_vec<T> operator*<>(const faust_core<T>& f, const faust_cu_vec<T>& v);
+	friend faust_cu_mat<T> operator*<>(const faust_core<T>& f, const faust_cu_mat<T>& M);
+	// friend void multiply<>(const faust_core<T> & A, const faust_cu_mat<T> & B, faust_cu_mat<T> & C,const T & alpha, char typeA, char typeMult);
+
+		
+};
+
+#include "faust_core.hpp"
+
+#endif
---
+++ faust_core/faust_core_cu/faust_core_cu.hpp
@@ -0,0 +1,342 @@
+#ifndef __FAUST_CORE_CU_HPP__
+#define __FAUST_CORE_CU_HPP__
+
+#include "faust_core_cu.h"
+#include "faust_cu_vec.h"
+//#include "hierarchical_fact.h"
+//#include "faust_params.h"
+#include "LinAlgebra_cu.h"
+#include "faust_core_algebra_cu.h"
+#include <iostream>
+#include "faust_exception.h"
+#include <fstream>
+
+using namespace std;
+template<typename T>
+const char * faust_core_cu<T>::class_name="faust_core_cu<T>::";
+
+template<typename T>
+faust_core_cu<T>::faust_core_cu() :
+   data(std::vector<faust_cu_spmat<T> >()),
+   totalNonZeros(0)
+{}
+
+template<typename T>
+faust_core_cu<T>::faust_core_cu(const faust_core_cu<T> & A) :
+   data(A.data),
+   totalNonZeros(A.totalNonZeros)
+{}
+
+template<typename T>
+faust_core_cu<T>::faust_core_cu(const std::vector<faust_cu_spmat<T> >& facts, const T lambda_ /* =1.0 */) :
+   data(facts),
+   totalNonZeros(0)
+{
+   for (int i=0 ; i<data.size() ; i++)
+      totalNonZeros += data[i].getNonZeros();
+
+   if(lambda_ != 1.0 && data.size()>0)
+      (data[0]) *= lambda_;
+}
+
+template<typename T>
+void faust_core_cu<T>::get_facts(std::vector<faust_cu_mat<T> >& facts)const
+{
+	facts.resize(size());
+	for (int i=0;i<size();i++)
+		facts[i] = data[i];
+	
+}
+
+template<typename T>
+void faust_core_cu<T>::print_file(const char* filename) const
+{
+	if (size() > 0)
+	{
+		ofstream fichier;
+		fichier.open(filename);	
+		fichier<<size()<<endl<<endl;
+		fichier.close();
+		
+		for (int i=0;i<size();i++)
+			data[i].print_file(filename,std::fstream::app);
+	}	
+}
+
+template<typename T>
+void faust_core_cu<T>::scalarMultiply(const T scalar)
+{
+	if (size() > 0)	
+		data[0]*=scalar;
+	else
+		handleError(class_name,"scalarMultiply : empty faust can't be multiplied ");
+}
+
+template<typename T>
+void faust_core_cu<T>::init_from_file(const char* filename) 
+{
+	clear();
+	FILE* fp=fopen(filename,"r");
+	int size_tmp;
+	fscanf(fp,"%d\n", &size_tmp);
+	fscanf(fp,"\n");
+	faust_cu_spmat<T> spmat;
+	std::cout<<"size_tmp"<<size_tmp<<std::endl;
+	for (int i=0;i<size_tmp;i++)
+	{
+		spmat.init_from_file(fp);
+      faust_cu_spmat<T> cu_spmat(spmat);
+		data.push_back(cu_spmat);
+	}
+	fscanf(fp,"\n");
+	updateNonZeros();
+}
+
+template<typename T>
+void faust_core_cu<T>::updateNonZeros()
+{
+	int totalNonZeros_tmp=0;
+	for (int i=0;i<size();i++)
+		totalNonZeros+=data[i].getNonZeros();
+}
+
+
+template<typename T>
+faust_core_cu<T>::faust_core_cu(const std::vector<faust_cu_mat<T> >&facts) :
+ data(std::vector<faust_cu_spmat<T> >()),
+   totalNonZeros(0)
+{	
+	for (int i=0;i<facts.size();i++)
+	{
+		faust_cu_spmat<T> spfact(facts[i]);
+		data.push_back(spfact);
+		totalNonZeros += data[i].getNonZeros();
+	}
+}
+
+
+
+/*faust_core_cu<T>::faust_core_cu(const faust_params& params) :
+   data(std::vector<faust_cu_spmat<T>>()),
+   totalNonZeros(0)
+
+{
+   hierarchical_fact hier_fact(params);
+   hier_fact.compute_facts();
+   hier_fact.get_facts(data);
+   for (int i=0 ; i<data.size() ; i++)
+      totalNonZeros += data[i].getNonZeros();
+   (data[0]) *= hier_fact.get_lambda();
+
+}*/
+template<typename T>
+faust_cu_mat<T> faust_core_cu<T>::get_product()const
+{
+	//complexity of evaluating a faust_core_cu 
+	// from left to right is (dim1*total_nnz)
+	// from right to left is (dim2*total_nnz)	
+	if (size() == 0)
+		handleError(class_name,"get_product : empty faust_core_cu");						
+	faust_cu_mat<T> prod(data[0].getNbRow()); 
+	
+	if(getNbRow()<getNbCol())
+	{
+		prod.resize(getNbRow());
+		prod.setEyes();
+		for(int i=0 ; i<data.size() ; i++)
+		prod *= data[i];		
+	}else
+	{
+		prod.resize(getNbCol());
+		prod.setEyes();
+		for(int i=data.size()-1 ; i>=0 ; i--)
+		prod.multiplyLeft(data[i]);		
+	}
+	/*faust_cu_mat<T> prod;
+	if ( (data[0].getNonZeros()+getNbRow()*(totalNonZeros-data[0].getNonZeros())) < (data[size()-1].getNonZeros()+getNbCol()*(totalNonZeros-data[size()-1].getNonZeros())) )
+	{
+		prod = data[0];	
+		for(int i=1 ; i<data.size() ; i++)
+		prod *= data[i];	
+	}else
+	{	
+		prod = data[size()-1];	
+		for(int i=data.size()-2 ; i>=0 ; i--)
+		prod.multiplyLeft(data[i]);	
+	}	*/
+   
+   
+
+   return prod;
+}
+
+template<typename T>
+int faust_core_cu<T>::getNbRow() const 
+{
+	(size()!=0)?data[0].getNbRow():-1;
+	
+}
+template<typename T>
+int faust_core_cu<T>::getNbCol() const 
+{
+	(size()!=0)?data[size()-1].getNbCol():-1;	
+}
+
+template<typename T>
+T faust_core_cu<T>::spectralNorm(const int nbr_iter_max, T threshold, int &flag) const
+{
+	if (size() == 0)
+		return 1;
+	else
+	{
+		faust_core_cu AtA(*this);
+		AtA.transpose();
+		if (getNbCol() < getNbRow())
+			AtA.multiply(*this);
+		else
+			AtA.multiplyLeft(*this);	
+		return std::sqrt(power_iteration(AtA,nbr_iter_max,threshold,flag));	
+	}	
+}
+
+template<typename T>
+void faust_core_cu<T>::multiply(const faust_core_cu & A)
+{
+	if (A.size() == 0)
+	{	
+	}
+   else
+	{
+		if (size() == 0)
+			(*this)=A;
+		else
+		{
+			if (getNbCol() != A.getNbRow())
+				handleError(class_name,"multiply : dimensions of the 2 faustcore are in conflict");
+			data.insert(data.end(),A.data.begin(),A.data.end());totalNonZeros+=A.totalNonZeros;	
+		}
+	}	
+}
+	
+template<typename T>	
+void faust_core_cu<T>::multiplyLeft(const faust_core_cu & A)
+{
+	if (A.size() == 0)
+	{		
+	}
+   else
+	{
+		if (size() == 0)
+			(*this)=A;
+		else
+		{
+			if (getNbRow() != A.getNbCol())
+				handleError(class_name,"multiplyLeft : dimensions of the 2 faustcore are in conflict");
+			data.insert(data.begin(),A.data.begin(),A.data.end());totalNonZeros+=A.totalNonZeros;	
+		}
+	}	
+}
+
+template<typename T>
+faust_cu_spmat<T> faust_core_cu<T>::get_fact(int id)const
+{
+	if((id>=size())||(id<0))
+	{
+		cout<<"id_fact error : "<<id<<endl;	
+		handleError(class_name,"get_fact : id exceed faust_core_cu size or id < 0"); 
+	}
+	cout<<"size_fact"<<size()<<endl;
+	cout<<"id_fact"<<id<<endl;
+	
+	return data[id];
+}
+
+template<typename T>
+void faust_core_cu<T>::push_back(const faust_cu_spmat<T>& S)
+{
+   if (size()>0)
+      if(data[size()-1].getNbCol()!=S.getNbRow() || S.getNbRow()<1)
+      handleError(class_name,"push_back : incorrect dimensions"); 
+   data.push_back(S);
+   totalNonZeros += S.getNonZeros();
+}
+
+
+template<typename T>
+void faust_core_cu<T>::push_first(const faust_cu_spmat<T>& S)
+{
+   if (size()>0)
+      if(data[0].getNbRow()!=S.getNbCol() || S.getNbRow()<1)
+         handleError(class_name,"push_first : incorrect dimensions"); 
+   data.insert(data.begin(),S);
+   totalNonZeros += S.getNonZeros();
+}
+
+template<typename T>
+void faust_core_cu<T>::pop_back(faust_cu_spmat<T>& S)
+{
+	if (size()>0)
+	{
+		S = data[size()-1];
+		data.pop_back();
+		totalNonZeros -= S.getNonZeros();
+	}
+	handleWarning("faust_core_cu<T>::pop_back : empty faust_core_cu");	
+}
+
+template<typename T>
+void faust_core_cu<T>::pop_first(faust_cu_spmat<T>& S)
+{
+	if (size()>0)
+	{
+		S = data[0];
+		data.erase(data.begin());
+		totalNonZeros -= S.getNonZeros();
+	}
+	handleWarning("faust_core_cu<T>::pop_back : empty faust_core_cu");		
+}
+
+template<typename T>
+void faust_core_cu<T>::pop_first(faust_cu_spmat<T>& S) const 
+{
+	if (size()>0)
+	{
+		S = data[0];
+		//data.erase(data.begin());
+		//totalNonZeros -= S.getNonZeros();
+	}
+		
+}
+
+template<typename T>
+void faust_core_cu<T>::transpose()
+{	
+	int nb_fact=size();
+	reverse(data.begin(),data.end());
+	for (int i=0;i<nb_fact;i++)
+		data[i].transpose();
+}
+
+
+
+
+template<typename T>
+void faust_core_cu<T>::Display()const
+{
+   
+	cout << "SIZE" << size() <<endl;
+   for (int i=0 ; i<size() ; i++)
+   {
+      if(data[i].getNbCol()>20)
+         cout << "data["<<i<<"] = "<< data[i].getNbRow() <<"x"<< data[i].getNbCol()<<" sparse matrix with "<<data[i].getNonZeros()<<" non-zero values"<<endl;
+      else
+      {
+         cout << "data["<<i<<"] = "<<endl;
+         data[i].Display();
+         cout<<endl;
+      }
+   }	
+   cout<<endl;   
+}
+
+#endif
