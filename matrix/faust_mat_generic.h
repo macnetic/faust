b13608d8d0e5aef6de313b87769098361eaf82e5
Adrien Leman <adrien.leman@inria.fr>
2016-03-01 16:21:18 +0000

    DOC definition classes

--- matrix/faust_mat_generic.h
+++ matrix/faust_mat_generic.h
@@ -3,25 +3,25 @@
 
 #include "faust_constant.h"
 
-
+/**
+ * \class faust_mat_generic faust_mat_generic.h
+ * \brief This faust_mat_generic class serves as a base class for the derived class faust_mat and faust_spmat .
+ * 	Member variable dim1 and dim2 correspond to the dimension of the matrix
+ *
+*/
 class faust_mat_generic
 {
-public:	
+	public:	
+	faust_mat_generic() : dim1(0), dim2(0) {}
+	faust_mat_generic(faust_unsigned_int dim1_, faust_unsigned_int dim2_) : dim1(dim1_), dim2(dim2_){}
 
-faust_mat_generic() : dim1(0), dim2(0) {}
-faust_mat_generic(faust_unsigned_int dim1_, faust_unsigned_int dim2_) : dim1(dim1_), dim2(dim2_){}
+	faust_unsigned_int getNbRow() const {return dim1;}
+	faust_unsigned_int getNbCol() const {return dim2;} 
+	
+	void resize(const faust_unsigned_int dim1_,const faust_unsigned_int dim2_){dim1=dim1_;dim2=dim2_;}
 
-
-faust_unsigned_int getNbRow() const {return dim1;}
-faust_unsigned_int getNbCol() const {return dim2;} 
-void resize(const faust_unsigned_int dim1_,const faust_unsigned_int dim2_){dim1=dim1_;dim2=dim2_;}
-
-protected:
-
+	protected:
 	faust_unsigned_int dim1;
-    faust_unsigned_int dim2;
-	
-	
+   	faust_unsigned_int dim2;	
 };
-
 #endif
