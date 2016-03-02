923ad5ddb98256f9358a4a9917f271037e457566
Adrien Leman <adrien.leman@inria.fr>
2016-03-02 17:35:58 +0000

    DOC class

--- matrix/faust_vec.h
+++ matrix/faust_vec.h
@@ -8,14 +8,12 @@
 #include "faust_exception.h"
 
 #ifdef __COMPILE_TIMERS__
-  #include "faust_timer.h"
+	#include "faust_timer.h"
 #endif
 
 template<typename T> class faust_mat;
 template<typename T> class faust_spmat;
 template<typename T> class faust_vec;
-
-
 
 template<typename T> 
 void gemv(const faust_mat<T> & A,const faust_vec<T> & x,faust_vec<T> & y,const T & alpha, const T & beta, char typeA);
@@ -26,83 +24,84 @@ faust_vec<T> solve(const faust_mat<T> & A, const faust_vec<T> & v);
 template<typename T> 
 void sp_solve(const faust_mat<T> & A,faust_vec<T> & x, const faust_vec<T> & y);
 
-
-
+/*! \class faust_vec faust_vec.h 
+* \brief Class template representing dense vector <br>   
+* This class implements basic linear algebra operation (addition, multiplication, frobenius and spectral norm...) <br>
+*/
 template<typename T>
 class faust_vec
 {
-	
 	template<class> friend class faust_vec;
- public :
- faust_vec() : dim(0), vec() {}
- faust_vec(const int _dim) : dim(_dim), vec(_dim){}
- // template<typename U>
- // faust_vec(const faust_vec<U>& v) : dim(v.dim), vec(v.vec){}
-  template<typename U>
- faust_vec(const faust_vec<U>& v);
- faust_vec(const faust_vec<T>& v) : dim(v.dim), vec(v.vec){}
- faust_vec(const faust_unsigned_int dim_, const T* data_);
 	
- T* getData(){return vec.data();}
- const T* getData() const {return vec.data();}
- void setOnes();
- void Display() const;
-void print_file(const char* filename)const;
- faust_unsigned_int size() const {return dim;}
- void resize(const int new_dim);
-T norm(){return vec.norm();}
-void scalarMultiply(T const scalar){vec *= scalar;}
-void normalize(){scalarMultiply(1/norm());} 
+	public :
+	faust_vec() : dim(0), vec() {}
+	faust_vec(const int _dim) : dim(_dim), vec(_dim){}
+
+	// template<typename U>
+	// faust_vec(const faust_vec<U>& v) : dim(v.dim), vec(v.vec){}
+	template<typename U>
+	faust_vec(const faust_vec<U>& v);
+	faust_vec(const faust_vec<T>& v) : dim(v.dim), vec(v.vec){}
+	faust_vec(const faust_unsigned_int dim_, const T* data_);
+	
+	T* getData(){return vec.data();}
+	
+	const T* getData() const {return vec.data();}
+	void setOnes();
+	void Display() const;
+	void print_file(const char* filename)const;
+	
+	faust_unsigned_int size() const {return dim;}
+	void resize(const int new_dim);
+	T norm(){return vec.norm();}
+	void scalarMultiply(T const scalar){vec *= scalar;}
+	void normalize(){scalarMultiply(1/norm());} 
 
 
-// multiply (*this) =  A * (*this)
-void  multiplyLeft(faust_mat<T> const& A){gemv<T>(A, *this, *this, 1.0, 0.0, 'N');}
-void  multiplyLeft(faust_spmat<T> const& A);
-  
-T sum()const{return vec.sum();}
-T mean()const{return vec.mean();}
-T dot(const faust_vec<T>& v)const;
+	// multiply (*this) =  A * (*this)
+	void  multiplyLeft(faust_mat<T> const& A){gemv<T>(A, *this, *this, 1.0, 0.0, 'N');}
+	void  multiplyLeft(faust_spmat<T> const& A);
+ 
+	T sum()const{return vec.sum();}
+	T mean()const{return vec.mean();}
+	T dot(const faust_vec<T>& v)const;
+
+	template<typename U>
+	void operator=(faust_vec<U> const& y);
+	void operator=(faust_vec<T> const& y);
+
+	void operator*=(const T alpha);
+	void operator+=(const T alpha);
+	void operator-=(const T alpha);
+
+	void operator+=(const faust_vec<T>& v);
+	void operator-=(const faust_vec<T>& v);
+
+	T mean_relative_error(const faust_vec<T>& v);
+
+	T& operator[](faust_unsigned_int i){return vec(i);}
+	const T& operator[](faust_unsigned_int i)const{return vec(i);}
+
+	const T& operator()(faust_unsigned_int i)const{return vec(i);}
 
 
-template<typename U>
-void operator=(faust_vec<U> const& y);
-void operator=(faust_vec<T> const& y);
+	// friend algebra
+	friend void gemv<>(const faust_mat<T> & A,const faust_vec<T> & x,faust_vec<T> & y,const T & alpha, const T & beta, char typeA);
+	friend faust_vec<T> solve<>(const faust_mat<T> & A, const faust_vec<T> & v);
+	friend void sp_solve<>(const faust_mat<T> & A,faust_vec<T> & x, const faust_vec<T> & y);
 
-void operator*=(const T alpha);
-void operator+=(const T alpha);
-void operator-=(const T alpha);
-
-void operator+=(const faust_vec<T>& v);
-void operator-=(const faust_vec<T>& v);
-
-
-T mean_relative_error(const faust_vec<T>& v);
-
-T& operator[](faust_unsigned_int i){return vec(i);}
-const T& operator[](faust_unsigned_int i)const{return vec(i);}
-
-const T& operator()(faust_unsigned_int i)const{return vec(i);}
-
-
-// friend algebra
-friend void gemv<>(const faust_mat<T> & A,const faust_vec<T> & x,faust_vec<T> & y,const T & alpha, const T & beta, char typeA);
-friend faust_vec<T> solve<>(const faust_mat<T> & A, const faust_vec<T> & v);
-friend void sp_solve<>(const faust_mat<T> & A,faust_vec<T> & x, const faust_vec<T> & y);
-
-private:
-  faust_unsigned_int dim;
-  static const char * class_name;
-  Eigen::Matrix<T, Eigen::Dynamic,1> vec;
+	private:
+	faust_unsigned_int dim;
+	static const char * class_name;
+	Eigen::Matrix<T, Eigen::Dynamic,1> vec;
 
 	#ifdef __COMPILE_TIMERS__
 		public: 
-			faust_timer t_local_multiplyLeft;
+		faust_timer t_local_multiplyLeft;
 	#endif
-  
+
 };
 
-
 #include "faust_vec.hpp"
-
 
 #endif
