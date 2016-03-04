406fd5decbf4777300577c8bad6b1fa43d73ab75
Adrien Leman <adrien.leman@inria.fr>
2016-03-04 16:28:00 +0000

    DOC faust_core

--- faust_core/faust_core.hpp
+++ faust_core/faust_core.hpp
@@ -15,28 +15,29 @@ using namespace std;
 template<typename T>
 const char * faust_core<T>::class_name="faust_core<T>::";
 
+
 template<typename T>
 faust_core<T>::faust_core() :
-   data(std::vector<faust_spmat<T> >()),
-   totalNonZeros(0)
+	data(std::vector<faust_spmat<T> >()),
+	totalNonZeros(0)
 {}
 
 template<typename T>
 faust_core<T>::faust_core(const faust_core<T> & A) :
-   data(A.data),
-   totalNonZeros(A.totalNonZeros)
+	data(A.data),
+	totalNonZeros(A.totalNonZeros)
 {}
 
 template<typename T>
 faust_core<T>::faust_core(const std::vector<faust_spmat<T> > & facts, const T lambda_ /* =1.0 */) :
-   data(facts),
-   totalNonZeros(0)
+    data(facts),
+	totalNonZeros(0)
 {
-   for (int i=0 ; i<data.size() ; i++)
-      totalNonZeros += data[i].getNonZeros();
+	for (int i=0 ; i<data.size() ; i++)
+		totalNonZeros += data[i].getNonZeros();
 
-   if(lambda_ != 1.0 && data.size()>0)
-      (data[0]) *= lambda_;
+	if(lambda_ != 1.0 && data.size()>0)
+		(data[0]) *= lambda_;
 }
 
 template<typename T>
@@ -45,9 +46,7 @@ void faust_core<T>::get_facts(std::vector<faust_mat<T> >& facts)const
 	facts.resize(size());
 	for (int i=0;i<size();i++)
 		facts[i] = data[i];
-	
 }
-
 
 
 
@@ -57,28 +56,28 @@ void faust_core<T>::print_file(const char* filename) const
 	if (size() > 0)
 	{
 		ofstream fichier;
-		fichier.open(filename);	
+		fichier.open(filename);
 		fichier<<size()<<endl<<endl;
 		fichier.close();
-		
+
 		for (int i=0;i<size();i++)
 		{
 			data[i].print_file(filename,std::fstream::app);
 		}
-	}	
+	}
 }
 
 template<typename T>
 void faust_core<T>::scalarMultiply(const T scalar)
 {
-	if (size() > 0)	
+	if (size() > 0)
 		data[0]*=scalar;
 	else
 		handleError(class_name,"scalarMultiply : empty faust can't be multiplied ");
 }
 
 template<typename T>
-void faust_core<T>::init_from_file(const char* filename) 
+void faust_core<T>::init_from_file(const char* filename)
 {
 	clear();
 	FILE* fp=fopen(filename,"r");
@@ -107,9 +106,9 @@ void faust_core<T>::updateNonZeros()
 
 template<typename T>
 faust_core<T>::faust_core(const std::vector<faust_mat<T> >&facts) :
- data(std::vector<faust_spmat<T> >()),
-   totalNonZeros(0)
-{	
+	data(std::vector<faust_spmat<T> >()),
+	totalNonZeros(0)
+{
 	faust_spmat<T> spfact;
 	for (int i=0;i<facts.size();i++)
 	{
@@ -137,48 +136,48 @@ faust_core<T>::faust_core(const std::vector<faust_mat<T> >&facts) :
 template<typename T>
 faust_mat<T> faust_core<T>::get_product()const
 {
-	//complexity of evaluating a faust_core 
+	//complexity of evaluating a faust_core
 	// from left to right is (dim1*total_nnz)
-	// from right to left is (dim2*total_nnz)	
+	// from right to left is (dim2*total_nnz)
 	if (size() == 0)
 	{
-		handleError(class_name,"get_product : empty faust_core");						
-	}	
-	faust_mat<T> prod(data[0].getNbRow()); 
-	
+		handleError(class_name,"get_product : empty faust_core");
+	}
+	faust_mat<T> prod(data[0].getNbRow());
+
 	if(getNbRow()<getNbCol())
 	{
 		prod.resize(getNbRow());
 		prod.setEyes();
 		for(int i=0 ; i<data.size() ; i++)
-		prod *= data[i];		
+            prod *= data[i];
 	}else
 	{
 		prod.resize(getNbCol());
 		prod.setEyes();
 		for(int i=data.size()-1 ; i>=0 ; i--)
-		prod.multiplyLeft(data[i]);		
+			prod.multiplyLeft(data[i]);
 	}
 	/*faust_mat<T> prod;
 	if ( (data[0].getNonZeros()+getNbRow()*(totalNonZeros-data[0].getNonZeros())) < (data[size()-1].getNonZeros()+getNbCol()*(totalNonZeros-data[size()-1].getNonZeros())) )
 	{
-		prod = data[0];	
+		prod = data[0];
 		for(int i=1 ; i<data.size() ; i++)
-		prod *= data[i];	
+		prod *= data[i];
 	}else
-	{	
-		prod = data[size()-1];	
+	{
+		prod = data[size()-1];
 		for(int i=data.size()-2 ; i>=0 ; i--)
-		prod.multiplyLeft(data[i]);	
+		prod.multiplyLeft(data[i]);
 	}	*/
-   
-   
+
+
 
    return prod;
 }
 
 template<typename T>
-int faust_core<T>::getNbRow() const 
+int faust_core<T>::getNbRow() const
 {
 	if (size() != 0)
 	{
@@ -187,10 +186,11 @@ int faust_core<T>::getNbRow() const
 	{
 		return -1;
 	}
-	
+
 }
+
 template<typename T>
-int faust_core<T>::getNbCol() const 
+int faust_core<T>::getNbCol() const
 {
 	if (size() != 0)
 	{
@@ -199,7 +199,6 @@ int faust_core<T>::getNbCol() const
 	{
 		return -1;
 	}
-	
 }
 
 template<typename T>
@@ -214,21 +213,13 @@ T faust_core<T>::spectralNorm(const int nbr_iter_max, T threshold, int &flag) co
 		AtA.transpose();
 		if (getNbCol() < getNbRow())
 		{
-			
 			AtA.multiply((*this));
-			
 		}else
 		{
-			AtA.multiplyLeft((*this));	
-		}		
+			AtA.multiplyLeft((*this));
+		}
 		return std::sqrt(power_iteration(AtA,nbr_iter_max,threshold,flag));
-		
-		
-		
-	}	
-	
-	
-	
+	}
 }
 
 template<typename T>
@@ -236,7 +227,7 @@ void faust_core<T>::multiply(const faust_core & A)
 {
 	if (A.size() == 0)
 	{
-		
+
 	}else
 	{
 		if (size() == 0)
@@ -249,18 +240,18 @@ void faust_core<T>::multiply(const faust_core & A)
 			{
 				handleError(class_name,"multiply : dimensions of the 2 faustcore are in conflict");
 			}
-			data.insert(data.end(),A.data.begin(),A.data.end());totalNonZeros+=A.totalNonZeros;	
+			data.insert(data.end(),A.data.begin(),A.data.end());totalNonZeros+=A.totalNonZeros;
 		}
-	}	
 	}
-	
+}
 
-template<typename T>	
+
+template<typename T>
 void faust_core<T>::multiplyLeft(const faust_core & A)
 {
 	if (A.size() == 0)
 	{
-		
+
 	}else
 	{
 		if (size() == 0)
@@ -273,13 +264,10 @@ void faust_core<T>::multiplyLeft(const faust_core & A)
 			{
 				handleError(class_name,"multiplyLeft : dimensions of the 2 faustcore are in conflict");
 			}
-			data.insert(data.begin(),A.data.begin(),A.data.end());totalNonZeros+=A.totalNonZeros;	
+			data.insert(data.begin(),A.data.begin(),A.data.end());totalNonZeros+=A.totalNonZeros;
 		}
-	}	
 	}
-
-
-
+}
 
 
 
@@ -288,44 +276,40 @@ faust_spmat<T> faust_core<T>::get_fact(int id)const
 {
 	if((id>=size())||(id<0))
 	{
-		cout<<"id_fact error : "<<id<<endl;	
-		handleError(class_name,"get_fact : id exceed faust_core size or id < 0"); 
+		cout<<"id_fact error : "<<id<<endl;
+		handleError(class_name,"get_fact : id exceed faust_core size or id < 0");
 	}
 	cout<<"size_fact"<<size()<<endl;
 	cout<<"id_fact"<<id<<endl;
-	
+
 	return data[id];
 }
 
 template<typename T>
 void faust_core<T>::push_back(const faust_spmat<T>& S)
 {
-   if (size()>0)
-   {   
-      if(data[size()-1].getNbCol()!=S.getNbRow() || S.getNbRow()<1)
-      {
-		 handleError(class_name,"push_back : incorrect dimensions"); 
-      }
-   }
-   data.push_back(S);
-   totalNonZeros += S.getNonZeros();
-
-	
+	if (size()>0)
+	{
+		if(data[size()-1].getNbCol()!=S.getNbRow() || S.getNbRow()<1)
+      		{
+			handleError(class_name,"push_back : incorrect dimensions");
+     		}
+   	}
+   	data.push_back(S);
+   	totalNonZeros += S.getNonZeros();
 }
 
 
 template<typename T>
 void faust_core<T>::push_first(const faust_spmat<T>& S)
 {
-   if (size()>0)
-      if(data[0].getNbRow()!=S.getNbCol() || S.getNbRow()<1)
-      {	
-		handleError(class_name,"push_first : incorrect dimensions"); 
-      }
-   data.insert(data.begin(),S);
-   totalNonZeros += S.getNonZeros();
-
-	
+	if (size()>0)
+		if(data[0].getNbRow()!=S.getNbCol() || S.getNbRow()<1)
+      		{
+			handleError(class_name,"push_first : incorrect dimensions");
+      		}
+	data.insert(data.begin(),S);
+   	totalNonZeros += S.getNonZeros();
 }
 
 
@@ -338,7 +322,7 @@ void faust_core<T>::pop_back(faust_spmat<T>& S)
 		data.pop_back();
 		totalNonZeros -= S.getNonZeros();
 	}
-	handleWarning("faust_core<T>::pop_back : empty faust_core");	
+	handleWarning("faust_core<T>::pop_back : empty faust_core");
 }
 
 template<typename T>
@@ -350,11 +334,11 @@ void faust_core<T>::pop_first(faust_spmat<T>& S)
 		data.erase(data.begin());
 		totalNonZeros -= S.getNonZeros();
 	}
-	handleWarning("faust_core<T>::pop_back : empty faust_core");		
+	handleWarning("faust_core<T>::pop_back : empty faust_core");
 }
 
 template<typename T>
-void faust_core<T>::pop_first(faust_spmat<T>& S) const 
+void faust_core<T>::pop_first(faust_spmat<T>& S) const
 {
 	if (size()>0)
 	{
@@ -362,12 +346,12 @@ void faust_core<T>::pop_first(faust_spmat<T>& S) const
 		//data.erase(data.begin());
 		//totalNonZeros -= S.getNonZeros();
 	}
-		
+
 }
 
 template<typename T>
 void faust_core<T>::transpose()
-{	
+{
 	int nb_fact=size();
 	reverse(data.begin(),data.end());
 	for (int i=0;i<nb_fact;i++)
@@ -377,28 +361,23 @@ void faust_core<T>::transpose()
 }
 
 
-
-
-
-
 template<typename T>
 void faust_core<T>::Display()const
 {
-   
 	cout << "SIZE" << size() <<endl;
-   for (int i=0 ; i<size() ; i++)
-   {
-      if(data[i].getNbCol()>20)
-         cout << "data["<<i<<"] = "<< data[i].getNbRow() <<"x"<< data[i].getNbCol()<<" sparse matrix with "<<data[i].getNonZeros()<<" non-zero values"<<endl;
-      else
-      {
-         cout << "data["<<i<<"] = "<<endl;
-         data[i].Display();
-         cout<<endl;
-      }
-   }	
-   cout<<endl;
-   
+	for (int i=0 ; i<size() ; i++)
+   	{
+      		if(data[i].getNbCol()>20)
+         		cout << "data["<<i<<"] = "<< data[i].getNbRow() <<"x"<< data[i].getNbCol()<<" sparse matrix with "<<data[i].getNonZeros()<<" non-zero values"<<endl;
+      		else
+      		{
+         		cout << "data["<<i<<"] = "<<endl;
+         		data[i].Display();
+         		cout<<endl;
+      		}
+   	}
+   	cout<<endl;
 }
 
 #endif
+
