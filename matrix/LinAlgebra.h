ab0a777f8cc63c65258b61f16d8fb3b120d61204
Adrien Leman <adrien.leman@inria.fr>
2016-03-04 10:57:32 +0000

    DOC class

--- matrix/LinAlgebra.h
+++ matrix/LinAlgebra.h
@@ -3,23 +3,18 @@
 
 #include "faust_constant.h"
 
-
 template<typename T> class faust_vec;
 template<typename T> class faust_mat;
 
-
-
-
-
 //////////FONCTION faust_mat<T> - faust_mat<T> ////////////////////
-// C = A * B; 
+// C = A * B;
 //l'objet C doit etre different de A et B
-/*! \brief Multiplication C = A * B 
+/*! \brief Multiplication C = A * B
  * Object C must be different of A and B.
  * */
 template<typename T>
 void multiply(const faust_mat<T> & A, const faust_mat<T> & B, faust_mat<T> & C);
- 
+
 // dot product : sum_i(v1[i]*v2[i])
 /*template <typename T>
 T dot(const faust_vec<T>& v1, const faust_vec<T>& v2);*/
@@ -34,7 +29,7 @@ T dot(const faust_vec<T>& v1, const faust_vec<T>& v2);*/
 * \param C : Dense Matrix
 * \param alpha : Template coefficient
 * \param beta : Template coefficient
-*/ 
+*/
 template<typename T>
 void gemm(const faust_mat<T> & A,const faust_mat<T> & B, faust_mat<T> & C,const T & alpha, const T & beta, char  typeA, char  typeB);
 
@@ -49,20 +44,20 @@ void gemm(const faust_mat<T> & A,const faust_mat<T> & B, faust_mat<T> & C,const 
 * \param alpha : Template coefficient
 * \param beta : Template coefficient
 * \param typeA : char
-*/ 
+*/
 template<typename T>
 void gemv(const faust_mat<T> & A,const faust_vec<T> & x,faust_vec<T> & y,const T & alpha, const T & beta, char typeA);
 
 /*!
 *  \fn T power_iteration(const faust_mat<T> & A, const faust_unsigned_int nbr_iter_max,T threshold,faust_int & flag)
-*  \brief compute the biggest eigenvalue of A using power_iteration algorithm	
+*  \brief compute the biggest eigenvalue of A using power_iteration algorithm
 * 	\param[in] A : input matrix must be semidefinite positive
 *	\param[in] nbr_iter_max : maximum number of iteration
 *	\param[in] threshold : threshold until convergence
 *	\param[in,out] flag : convergence flag
-*	\return	the the biggest eigenvalue of A   
+*	\return	the the biggest eigenvalue of A
 */
-template<typename T> 
+template<typename T>
 T power_iteration(const faust_mat<T> & A, const faust_unsigned_int nbr_iter_max,T threshold,faust_int & flag);
 
 template<typename T>
