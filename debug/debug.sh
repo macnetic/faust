22c900ce291960b2eedf5d4a50d8c3608f8125fc
Thomas Gautrais <thomas.gautrais@inria.fr>
2016-03-15 08:42:04 +0000

    modif script debug

--- debug/debug.sh
+++ debug/debug.sh
@@ -12,16 +12,24 @@ cd $DEBUG_DIR
 
 rm -f *.tmp
 
-cd $BUILD_DIR_GPU
-make cleanall 
-make -j$OPENBLAS_NUM_THREADS  >> "$LOG_DIR/compilation_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log" 2>&1
-cd $DEBUG_DIR
-#$BUILD_DIR_GPU/hierarchical_fact_test_cu.out | tee -a "$LOG_DIR/faust_hier_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log"
-$BUILD_DIR_GPU/MEG_fact_cu.out | tee -a "$LOG_DIR/meg_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log"
+declare -a real_type=("float" "double")
 
-cd $BUILD_DIR_CPU 
-make clean
-make -j$OPENBLAS_NUM_THREADS  >> "$LOG_DIR/compilation_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log" 2>&1
-cd $DEBUG_DIR
-#$BUILD_DIR_CPU/testing/bin/faust_hier | tee -a "$LOG_DIR/faust_hier_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log"
-$BUILD_DIR_CPU/testing/bin/meg | tee -a "$LOG_DIR/meg_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log"
+
+for i in "${real_type[@]}"
+do
+	sed -i "s/typedef [[:alpha:]]* faust_real/typedef ${real_type[$i]} faust_real/" $BUILD_DIR_GPU/hierarchical_fact_test_cu.cpp $BUILD_DIR_GPU/MEG_fact_cu.cpp $BUILD_DIR_CPU/testing/src/hierarchical_fact_test.cpp $BUILD_DIR_CPU/testing/src/MEG_fact.cpp 
+
+	cd $BUILD_DIR_GPU
+	make cleanall 
+	make -j$OPENBLAS_NUM_THREADS  >> "$LOG_DIR/compilation_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log" 2>&1
+	cd $DEBUG_DIR
+	$BUILD_DIR_GPU/hierarchical_fact_test_cu.out | tee -a "$LOG_DIR/faust_hier_${real_type[$i]}_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log"
+	$BUILD_DIR_GPU/MEG_fact_cu.out | tee -a "$LOG_DIR/meg_${real_type[$i]}_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log"
+
+	cd $BUILD_DIR_CPU 
+	make clean
+	make -j$OPENBLAS_NUM_THREADS  >> "$LOG_DIR/compilation_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log" 2>&1
+	cd $DEBUG_DIR
+	$BUILD_DIR_CPU/testing/bin/faust_hier | tee -a "$LOG_DIR/faust_hier_${real_type[$i]}_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log"
+	$BUILD_DIR_CPU/testing/bin/meg | tee -a "$LOG_DIR/meg_${real_type[$i]}_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log"
+done
