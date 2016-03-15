b8659bc9d95b856d412ecbb123f436e295ff7c75
Adrien Leman <adrien.leman@inria.fr>
2016-03-15 10:23:23 +0000

    modif script debug.sh

--- debug/debug.sh
+++ debug/debug.sh
@@ -23,13 +23,13 @@ do
 	make cleanall 
 	make -j$OPENBLAS_NUM_THREADS  >> "$LOG_DIR/compilation_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log" 2>&1
 	cd $DEBUG_DIR
-	$BUILD_DIR_GPU/hierarchical_fact_test_cu.out | tee -a "$LOG_DIR/faust_hier_$precision_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log"
-#$BUILD_DIR_GPU/MEG_fact_cu.out | tee -a "$LOG_DIR/meg_$precision_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log"
+	$BUILD_DIR_GPU/hierarchical_fact_test_cu.out | tee -a "$LOG_DIR/faust_hier_${precision}_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log"
+	$BUILD_DIR_GPU/MEG_fact_cu.out | tee -a "$LOG_DIR/meg_${precision}_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_GPU.log"
 
 	cd $BUILD_DIR_CPU 
 	make clean
 	make -j$OPENBLAS_NUM_THREADS  >> "$LOG_DIR/compilation_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log" 2>&1
 	cd $DEBUG_DIR
-	$BUILD_DIR_CPU/testing/bin/faust_hier | tee -a "$LOG_DIR/faust_hier_$precision_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log"
-#$BUILD_DIR_CPU/testing/bin/meg | tee -a "$LOG_DIR/meg_$precision_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log"
+	$BUILD_DIR_CPU/testing/bin/faust_hier | tee -a "$LOG_DIR/faust_hier_${precision}_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log"
+	$BUILD_DIR_CPU/testing/bin/meg | tee -a "$LOG_DIR/meg_${precision}_`date '+%Y-%m-%d_%H-%M-%S'`_`hostname`_`whoami`_CPU.log"
 done
