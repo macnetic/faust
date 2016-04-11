0af802e0af87a56247f6db398496e2e00b40bbbe
Adrien Leman <adrien.leman@inria.fr>
2016-04-11 09:07:48 +0000

    ctest nightly add

---
+++ test/CTest_nightly/run_nightly_cormier.bash
@@ -0,0 +1,48 @@
+##################################################################
+####################### run_nightly ##############################
+# This shell-bash SCRIPT performs : 
+# 	- svn checkout
+#	- compile the selected target of faust project 
+#	- run some examples relating to these targets. 
+# 
+# faust_test directory contains the sources of the faust project. 
+# faust_test_build directory contains the build of faust project.
+# faust_output directory contains the build of faust project.
+#
+# WARNING : Please adjust your environment variables, corresponding 
+# to your computer configurations (library path) 
+#
+# svn checkout --username XXX https://scm.gforge.inria.fr/authscm/XXX/svn/faust/trunk/devcpp/test/CTest_nightly
+#
+###################################################################
+
+
+# Directory of the local path of the nightly project
+export PATH_DIR_RUN_NIGHTLY=/home/aleman/WORK/FAUST/faust_nightly
+
+
+# Directory of the library used in the FAUST PROJECT 
+export OPENBLASDIR=/opt/OpenBLAS
+export MATIODIR=/usr/local
+export CUDADIR=/usr/local/cuda-7.5
+export EIGENDIR=/usr/local/include/eigen3
+
+#export HDF5_ROOT_DIR=/home/aleman/Library/hdf5-1.8.16/src/.libs
+
+# CTEST OPTION 
+export CTEST_CONFIGURE_OPTIONS=
+
+
+cd $PATH_DIR_RUN_NIGHTLY
+rm -rf faust_test
+rm -rf faust_test_build
+rm -rf faust_output
+
+mkdir faust_test
+mkdir faust_test_build
+mkdir faust_output
+
+# launch ctest file 
+ctest -VV -C Release -S faustTest.cmake 
+
+
