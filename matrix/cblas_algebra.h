699c9bfeb3794269e67d89fcd875f5c4e232a598
Adrien Leman <adrien.leman@inria.fr>
2016-04-19 09:59:02 +0000

    doc cblas algebra

--- matrix/cblas_algebra.h
+++ matrix/cblas_algebra.h
@@ -6,6 +6,7 @@
 #include <iostream>
 
 /*! \brief cblas_gemm call the function cblas_sgemm or cblas_dgemm, depend to the precision (double or float), from the external library openBLAS    <br> 
+* This function is used to multiply the matrices.
 */
 template<typename T>
 void cblas_gemm(const CBLAS_ORDER order,const CBLAS_TRANSPOSE transa, const CBLAS_TRANSPOSE transb, const int dim1,const int dim2,const int nbColOpA, const T alpha, const T* Adata,const int lda,const T* Bdata,const int ldb, const T beta, T* Cdata, const int ldc);
@@ -14,6 +15,7 @@ void cblas_gemm(const CBLAS_ORDER order,const CBLAS_TRANSPOSE transa, const CBLA
 
 
 /*! \brief cblas_gemv call the function cblas_sgemv or cblas_dgemv, depend to the precision (double or float), from the external library openBLAS    <br> 
+* This function is used to multiply the matrices.
 */
 template<typename T>
 void cblas_gemv(const CBLAS_ORDER order,const CBLAS_TRANSPOSE transa,const int dim1,const int dim2,const T alpha,const T* Adata,const int lda,const T* Xdata,const int incX,const T beta,T* Ydata,const int incY);
