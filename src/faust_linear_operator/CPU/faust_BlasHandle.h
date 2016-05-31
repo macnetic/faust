ea998757fbaf0ead33924dd05457bf5f9bc80ee6
Adrien Leman <adrien.leman@inria.fr>
2016-05-31 15:47:11 +0000

    modifying file name

---
+++ src/faust_linear_operator/CPU/faust_BlasHandle.h
@@ -0,0 +1,33 @@
+#ifndef BLASHANDLE_CPU_H
+#define BLASHANDLE_CPU_H
+#include "faust_constant.h"
+
+
+namespace Faust
+{
+
+    template <Device DEVICE> class BlasHandle;
+
+    //BlasHandle<Cpu> is useless,
+    // its unique purpose is for code factorisation with GPU code
+
+    template <>
+    class BlasHandle<Cpu>
+    {
+
+    public :
+
+        BlasHandle(){}
+        BlasHandle(const BlasHandle<Cpu> & blashandle){}
+        void operator=(BlasHandle<Cpu> const& blashandle){};
+        void Init(){}
+        void Destroy(){}
+
+
+
+    };
+}
+
+
+
+#endif
