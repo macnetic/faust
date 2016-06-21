d00e7b7436792df9966e956589ea80d7ea0a4ca9
Adrien Leman <adrien.leman@inria.fr>
2016-06-21 12:40:09 +0000

    test AL ctest

--- misc/CTest_nightly/CTestConfig.cmake
+++ misc/CTest_nightly/CTestConfig.cmake
@@ -4,3 +4,6 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash.inria.fr")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=faust")
 set(CTEST_DROP_SITE_CDASH TRUE)
+
+set(CTEST_FULL_OUTPUT TRUE)
+
