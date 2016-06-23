a950e61a6d00d9b9bdc022ff9765365f6612f6a3
Nicolas Bellot <nicolas.bellot@inria.fr>
2016-06-23 14:39:54 +0000

    demo dir suppression fichier inutile

--- misc/demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aleman/WORK/FAUST/faust/trunk/devcpp/misc/demo
-# Build directory: /home/aleman/WORK/FAUST/faust/trunk/devcpp/misc/demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
