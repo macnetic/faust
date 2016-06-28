5d0954a345bfc1891e94d8f3d9f898cf8e2e676d
Adrien Leman <adrien.leman@inria.fr>
2016-06-28 07:54:35 +0000

    modif for CTest

--- misc/test/src/hier_fact.m
+++ misc/test/src/hier_fact.m
@@ -1,7 +1,26 @@
 %% test the mexhierarchical_fact function in the hier fact configuration
+% 
+% addpath(['..' filesep 'tools']);
+% set_path;
+% 
+% load config_compared_hierarchical_fact;
+% [mexlambda,mexfact,fc]=launch_hierarchical_fact(params);
+
+function [testPass]=hier_fact(expectedLambda, expectedLambdaPrecision)
 
 addpath(['..' filesep 'tools']);
 set_path;
 
 load config_compared_hierarchical_fact;
 [mexlambda,mexfact,fc]=launch_hierarchical_fact(params);
+
+if (mexlambda >= (expectedLambda - expectedLambdaPrecision)  &&  (mexlambda <= (expectedLambda + expectedLambdaPrecision)) )
+    testPass=0; % le résultats est OK
+     disp('');
+    disp('Test is successful');
+else 
+    testPass=1; % le resutat n'est pas bon.
+    disp('');
+    disp('Test is NOT successful');
+    exit (FAILURE);
+end
