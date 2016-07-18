cdcb85ea393ade4b0f6d8dfcd21f4f19eb1ef82c
Nicolas Bellot <nicolas.bellot@inria.fr>
2016-07-18 15:43:29 +0000

    compatibilite mac

--- src/faust_linear_operator/CPU/faust_MatSparse.h
+++ src/faust_linear_operator/CPU/faust_MatSparse.h
@@ -201,8 +201,8 @@ namespace Faust
 
 
         //! *this = S * (*this)
-        friend void  Faust::Vect<FPP,Cpu>::multiplyLeft(MatSparse<FPP,Cpu> const& S,const char TransS='N');
-	friend void  Faust::MatDense<FPP,Cpu>::multiplyLeft(MatSparse<FPP,Cpu> const& S,const char TransS='N');
+        friend void  Faust::Vect<FPP,Cpu>::multiplyLeft(MatSparse<FPP,Cpu> const& S,const char TransS);
+	friend void  Faust::MatDense<FPP,Cpu>::multiplyLeft(MatSparse<FPP,Cpu> const& S,const char TransS);
 
         friend void Faust::multiply<>(const Faust::Transform<FPP,Cpu> & A, const Faust::MatDense<FPP,Cpu> & B, Faust::MatDense<FPP,Cpu> & C,const FPP & alpha, char typeA, char typeMult);
         friend void Faust::spgemm<>(const MatSparse<FPP,Cpu> & A,const Faust::MatDense<FPP,Cpu> & B, Faust::MatDense<FPP,Cpu> & C,const FPP & alpha, const FPP & beta, char  typeA, char  typeB);
