08e028358cf6f97f4607c4bc9c6c9a604b67afdc
Rémi Gribonval <remi.gribonval@inria.fr>
2016-07-20 08:07:33 +0000

    Ajout roadmap

---
+++ misc/demo/set_path.m
@@ -0,0 +1,4 @@
+addpath('');%% access to the mexfunction
+addpath('/Users/remi/Devel/faust/trunk/devcpp/misc/demo/tools');%% access to the tools.m function (sparsify toolbox, figure toolbox ...)
+addpath('/Users/remi/Devel/faust/trunk/devcpp/misc/data/mat/demoLuc');%% access to the data
+addpath('');%% access to matlab_faust.m (interface betwwen faust_core C++ class and matlab class)
