1fc5bfcdfda8c6e81a16ee09b49ca3bb7817a343
testcdash Nicolas Bellot <nicolas.bellot@inria.fr>
2016-08-25 09:43:19 +0000

    modif find matlab windows

--- CMake/findMatlab.cmake
+++ CMake/findMatlab.cmake
@@ -41,13 +41,40 @@ if(UNIX)
 	#message(STATUS "MATLAB_DIR_TMP 4 = ${MATLAB_DIR_TMP}")
 
 elseif(WIN32)
-	if ($ENV{MATLAB_EXE_DIR_TMP}} MATCHES matlab)
-		message(STATUS "MATLAB_DIR_TMP = $ENV{MATLAB_EXE_DIR_TMP}")
-		set(MATLAB_DIR_TMP $ENV{MATLAB_EXE_DIR_TMP})
+
+	exec_program("${CMAKE_SOURCE_DIR}/CMake/find_matlab_path.bat")
+	FILE(READ "${PROJECT_BINARY_DIR}/logPath.txt" contents)
+	#message(STATUS "contents=${contents}")
+	#STRING(REGEX REPLACE "\n" "" contents "${contents}")
+
+	# On enregistre la premiére ligne du fichier logPath.txt comme chemin de matlab.
+	# Si plusieurs versions de matlab, WARNING
+	string(REGEX REPLACE "(\n)[a-zA-Z0-9_/\\:.\n]+" "\\1" contents1 "${contents}")
+	string(REGEX REPLACE "\n" "" contents1 "${contents1}")
+
+	# On garde la 2éme ligne
+	string(REGEX REPLACE "(\n)[a-zA-Z0-9_/\\:.]+(\n)" "\\1" contents2tmp "${contents}")
+	string(REGEX REPLACE "(\n)[a-zA-Z0-9_/\\:.\n]+" "\\1" contents2 "${contents2tmp}")
+	string(REGEX REPLACE "\n" "" contents2 "${contents2}")
+	#message(STATUS "contents1=${contents1}")
+	#message(STATUS "contents2tmp=${contents2tmp}")
+	#message(STATUS "contents2=${contents2}")
+	set(MATLAB_EXE_DIR_TMP "${contents1}")
+	set(MATLAB_EXE_DIR_TMP2 "${contents2}")
+	message(STATUS "MATLAB_EXE_DIR_TMP=${MATLAB_EXE_DIR_TMP}")
+	message(STATUS "MATLAB_EXE_DIR_TMP2=${MATLAB_EXE_DIR_TMP2}")
+	message(STATUS "If you want to choose an other version of Matlab, please add environment variable MATLAB_EXE_DIR ")
+			
+	if ($ENV{MATLAB_EXE_DIR}} MATCHES matlab)
+		#message(STATUS "MATLAB_DIR_TMP = $ENV{MATLAB_EXE_DIR_TMP}")
+		set(MATLAB_DIR_TMP $ENV{MATLAB_EXE_DIR})
+	elseif (${MATLAB_EXE_DIR_TMP} MATCHES matlab)
+		#message(STATUS "MATLAB_DIR_TMP = ${MATLAB_EXE_DIR_TMP}")
+		set(MATLAB_DIR_TMP ${MATLAB_EXE_DIR_TMP})
 	else()
-		message(STATUS "MATLAB_DIR_TMP = $ENV{MATLAB_EXE_DIR_TMP}")
+		message(STATUS "MATLAB_DIR_TMP = $ENV{MATLAB_EXE_DIR}")
 		message(STATUS "MATLAB_DIR_TMP is not available. It corresponds to the path of matlab.exe }")
-		message(FATAL_ERROR "Unknown path of matlab.exe. Please launch ./CMake/find_matlab_path.bat before cmake command. ")
+		message(FATAL_ERROR "Unknown path of matlab.exe. Please add environment variable MATLAB_EXE_DIR ")
 	endif()
 	
 	#message(STATUS "where /R \"C:\\Program Files\\MATLAB\" matlab.exe")
--- CMake/find_matlab_path.bat
+++ CMake/find_matlab_path.bat
@@ -1,14 +1,19 @@
 @echo off
 set "find_exe=matlab.exe"
 
-(where matlab.exe) > logPath.txt 
+(where matlab.exe) >> logPath.txt 
 (where /R "C:\\Program Files\\MATLAB" matlab.exe) >> logPath.txt 
 (where /R "C:\\Program Files (x86)\\MATLAB" matlab.exe) >> logPath.txt
 
+REM set MATLAB_EXE_DIR_TMP=
+REM set /p MATLAB_EXE_DIR_TMP=<logPath.txt
 
-set MATLAB_EXE_DIR_TMP=
-set /p MATLAB_EXE_DIR_TMP=<logPath.txt
-
-REM for /f "delims=" %%i in ('type logPath.txt') do (set MATLAB_DIR_TMP=%%i && echo %%i)
-
-echo environment variable is defined for matlab Path : %MATLAB_EXE_DIR_TMP%
+REM set /a cpt=1
+REM for /f "tokens=*" %%a In (logPath.txt) do (
+REM echo --
+REM echo %%a
+REM set MATLAB_EXE_DIR_TMP%cpt%=%%a
+REM set /a cpt=%cpt%+1
+REM echo -- 
+REM )
+REM echo environment variable is defined for matlab Path : %MATLAB_EXE_DIR_TMP%
