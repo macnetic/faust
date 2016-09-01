983c082515b04b4df67737136b9a8c46dfc28b90
Nicolas Bellot <nicolas.bellot@inria.fr>
2016-09-01 15:40:15 +0000

    wrapper/matlab/tools/ header

--- wrapper/matlab/tools/FaustCore.m
+++ wrapper/matlab/tools/FaustCore.m
@@ -1,13 +1,46 @@
 %class FAUSTCORE
 % this class only stored the Faust C++ object and 
 % inherit from matlab handle class which allows different
-% object to the same reference
+% object to have the same reference
 % 
 % WARNING : the user must not directly use this class,
 % he must use the matlab class Faust
 %
-% For more information on the FAuST Project, please visit the website of
+% For more information on the FAuST Project, please visit the website of 
 % the project :  <http://faust.gforge.inria.fr>
+%
+%% License:
+% Copyright (2016):	Nicolas Bellot, Adrien Leman, Thomas Gautrais, Luc Le Magoarou, Remi Gribonval
+%			INRIA Rennes, FRANCE
+%			http://www.inria.fr/
+%
+% The FAuST Toolbox is distributed under the terms of the GNU Affero 
+% General Public License.
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Affero General Public License as published 
+% by the Free Software Foundation.
+%
+% This program is distributed in the hope that it will be useful, but 
+% WITHOUT ANY WARRANTY; without even the implied warranty of 
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+% See the GNU Affero General Public License for more details.
+%
+% You should have received a copy of the GNU Affero General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%% Contacts:
+%   Nicolas Bellot	: nicolas.bellot@inria.fr
+%   Adrien Leman	: adrien.leman@inria.fr
+%   Thomas Gautrais : thomas.gautrais@inria.fr
+%	Luc Le Magoarou	: luc.le-magoarou@inria.fr
+%	Remi Gribonval	: remi.gribonval@inria.fr
+%
+%% References:
+% [1]	Le Magoarou L. and Gribonval R., "Flexible multi-layer sparse 
+%	approximations of matrices and applications", Journal of Selected 
+%	Topics in Signal Processing, 2016.
+%	<https://hal.archives-ouvertes.fr/hal-01167948v1>
+%%
 
 classdef FaustCore < handle
     properties (SetAccess = public, Hidden = false)
