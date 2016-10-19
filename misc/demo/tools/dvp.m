e825050db71e18a51d5d15d8dd489ec0bae651bb
Nicolas Bellot <nicolas.bellot@inria.fr>
2016-10-19 08:00:09 +0000

    header matlab dvp.m

--- misc/demo/tools/dvp.m
+++ misc/demo/tools/dvp.m
@@ -4,17 +4,41 @@ function Ddvp = dvp(D)
 %  which is the product of the matrices contained in D:
 %  Ddvp = D{1}*D{2}*...*D{n}.
 %
-%  References:
-%  [1] Le Magoarou L. and Gribonval R., "Learning computationally efficient
-%  dictionaries and their implementation as fast transforms", submitted to
-%  NIPS 2014
-
-%  Luc Le Magoarou
-%  PANAMA team
-%  Inria, Rennes - Bretagne Atlantique
-%  luc.le-magoarou@inria.fr
+% For more information on the FAuST Project, please visit the website of 
+% the project :  <http://faust.gforge.inria.fr>
 %
-%  June 2014
+%% License:
+% Copyright (2016):	Nicolas Bellot, Adrien Leman, Thomas Gautrais, 
+%			Luc Le Magoarou, Remi Gribonval
+%			INRIA Rennes, FRANCE
+%			http://www.inria.fr/
+%
+% The FAuST Toolbox is distributed under the terms of the GNU Affero 
+% General Public License.
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Affero General Public License as published 
+% by the Free Software Foundation.
+%
+% This program is distributed in the hope that it will be useful, but 
+% WITHOUT ANY WARRANTY; without even the implied warranty of 
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+% See the GNU Affero General Public License for more details.
+%
+% You should have received a copy of the GNU Affero General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+%% Contacts:
+%   Nicolas Bellot	: nicolas.bellot@inria.fr
+%   Adrien Leman	: adrien.leman@inria.fr
+%   Thomas Gautrais	: thomas.gautrais@inria.fr
+%   Luc Le Magoarou	: luc.le-magoarou@inria.fr
+%   Remi Gribonval	: remi.gribonval@inria.fr
+%
+%% References:
+% [1]	Le Magoarou L. and Gribonval R., "Flexible multi-layer sparse 
+%	approximations of matrices and applications", Journal of Selected 
+%	Topics in Signal Processing, 2016.
+%	<https://hal.archives-ouvertes.fr/hal-01167948v1>
 
 Nlayers = size(D,2);
 if Nlayers==0
