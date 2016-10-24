55e835f61c2357be4d79fc530c0185187479e733
testcdash Nicolas Bellot <nicolas.bellot@inria.fr>
2016-10-24 14:19:52 +0000

    test matlab sous windows

--- misc/test/tools/windows_test_matlab.bat
+++ misc/test/tools/windows_test_matlab.bat
@@ -1,38 +1,46 @@
 @ECHO OFF
 
 :: par defaut le test est bon
-set TESTPASS=0 
-set middle=%1
-set "debut= delete MATLAB_TEST.txt;diary MATLAB_TEST.txt;try;testpass=0;"
-set "fin= catch ME ;testpass=2;disp(getReport(ME)); end ; disp (testpass); diary OFF;exit(testpass);"
-set test=%debut%%middle%%fin%
+set ERRORLEVEL=0
+ 
+set matlab_command_core=%1
+set outputfile=%2%.txt
+::transform the "/" character into "\" to have a valid path
+set outputfile=%outputfile:/=\%
+
+echo "OUTPUTFILE %outputfile%"
+
+set "matlab_command_begin= delete %outputfile%;diary %outputfile%;try;testpass=0;"
+set "matlab_command_end= catch ME ;testpass=2;disp(getReport(ME));end; diary OFF;exit(testpass);"
+set matlab_command=%matlab_command_begin%%matlab_command_core%%matlab_command_end%
 ::echo DEBUT : %debut%
 ::echo MIDLLE : %middle%
 ::echo FIN : %fin%
 ::echo TEST : %test%
 
 ::transform the "@" character into "," 
-set test=%test:@=,%
+set matlab_command=%matlab_command:@=,%
 
 ::transform the "$" character into ";"
-set test=%test:$=;%
+set matlab_command=%matlab_command:$=;%
 
 
-echo MATLAB COMMAND : %test%
+echo MATLAB COMMAND : %matlab_command%
 
 :: launch the test in matlab an wait for matlab to finish
-matlab -nojvm -wait -r """%test%"""
+matlab -nojvm -wait -r """%matlab_command%"""
 
-:: si le code renvoyer par la derniere cmd est sup a 2 on met TESPASS a 2 et erreur
-IF ERRORLEVEL 2 set TESTPASS=2
+:: si le code renvoyer par la derniere cmd est sup a 2 => erreur
+IF ERRORLEVEL 2 set ERRORLEVEL=2
 
 
 
 
 :: display the content of the MATLAB Command Window stored in a txtfile
 echo ****** MATLAB WINDOW ******* 
-type MATLAB_TEST.txt
+type %outputfile%
+IF ERRORLEVEL 1 set ERRORLEVEL=1
 
-ECHO ERROR_LEVEL %TESTPASS%
+ECHO ERROR_LEVEL %ERRORLEVEL%
 ::renvoie le code erreur
-exit %TESTPASS%
+exit %ERRORLEVEL%
