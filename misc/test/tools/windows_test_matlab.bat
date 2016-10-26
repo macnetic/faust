ced8d267a9a8c6ba157f4ea419444537db6ef96c
testcdash Nicolas Bellot <nicolas.bellot@inria.fr>
2016-10-26 13:09:25 +0000

    header batch file

--- misc/test/tools/windows_test_matlab.bat
+++ misc/test/tools/windows_test_matlab.bat
@@ -1,3 +1,43 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::::                              Description:                                ::::
+::::  batch script to run matlab test. Take 2 arguments :                     ::::
+::::   -1st arg is a char representing the list of matlab command that will   :::: 
+::::      be executed														  ::::
+::::   -2nd arg is the name of the text file 								  ::::
+::::	where all the display in the matlab command window will be written in ::::
+::::    in order to display this file into the current terminal				  ::::	
+::::																		  ::::	 
+::::  For more information on the FAuST Project, please visit the website     ::::
+::::  of the project : <http://faust.gforge.inria.fr>                         ::::
+::::                                                                          ::::
+::::                              License:                                    ::::
+::::  Copyright (2016):   Nicolas Bellot, Adrien Leman, Thomas Gautrais,      ::::
+::::                      Luc Le Magoarou, Remi Gribonval                     ::::
+::::                      INRIA Rennes, FRANCE                                ::::
+::::                      http://www.inria.fr/                                ::::
+::::                                                                          ::::
+::::  The FAuST Toolbox is distributed under the terms of the GNU Affero      ::::
+::::  General Public License.                                                 ::::
+::::  This program is free software: you can redistribute it and/or modify    ::::
+::::  it under the terms of the GNU Affero General Public License as          ::::
+::::  published by the Free Software Foundation.                              ::::
+::::                                                                          ::::
+::::  This program is distributed in the hope that it will be useful, but     ::::
+::::  WITHOUT ANY WARRANTY; without even the implied warranty of              ::::
+::::  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    ::::
+::::  See the GNU Affero General Public License for more details.             ::::
+::::                                                                          ::::
+::::  You should have received a copy of the GNU Affero General Public        ::::
+::::  License along with this program.                                        ::::
+::::  If not, see <http://www.gnu.org/licenses/>.                             ::::
+::::                                                                          ::::
+::::                             Contacts:                                    ::::
+::::      Nicolas Bellot  : nicolas.bellot@inria.fr                           ::::
+::::      Adrien Leman    : adrien.leman@inria.fr                             ::::
+::::      Thomas Gautrais : thomas.gautrais@inria.fr                          ::::
+::::      Luc Le Magoarou : luc.le-magoarou@inria.fr                          ::::
+::::      Remi Gribonval  : remi.gribonval@inria.fr                           ::::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @ECHO OFF
 
 :: par defaut le test est bon
@@ -19,6 +59,10 @@ set matlab_command=%matlab_command_begin%%matlab_command_core%%matlab_command_en
 ::echo TEST : %test%
 
 ::transform the "@" character into "," 
+
+
+
+
 set matlab_command=%matlab_command:@=,%
 
 ::transform the "$" character into ";"
