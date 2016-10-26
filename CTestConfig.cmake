8eadf2809cbeb9a7a147ccb1d7e14a0a24c393d3
Nicolas Bellot <nicolas.bellot@inria.fr>
2016-10-26 14:44:42 +0000

    header cmake

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,3 +1,40 @@
+##############################################################################
+##                              Description:                                ##
+##  Cmake file to configure the test that will be send to CDash plateform   ##
+##									    ##                ##                                                                          ##							
+##  For more information on the FAuST Project, please visit the website     ##
+##  of the project : <http://faust.gforge.inria.fr>                         ##
+##                                                                          ##
+##                              License:                                    ##
+##  Copyright (2016):   Nicolas Bellot, Adrien Leman, Thomas Gautrais,      ##
+##                      Luc Le Magoarou, Remi Gribonval                     ##
+##                      INRIA Rennes, FRANCE                                ##
+##                      http://www.inria.fr/                                ##
+##                                                                          ##
+##  The FAuST Toolbox is distributed under the terms of the GNU Affero      ##
+##  General Public License.                                                 ##
+##  This program is free software: you can redistribute it and/or modify    ##
+##  it under the terms of the GNU Affero General Public License as          ##
+##  published by the Free Software Foundation.                              ##
+##                                                                          ##
+##  This program is distributed in the hope that it will be useful, but     ##
+##  WITHOUT ANY WARRANTY; without even the implied warranty of              ##
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    ##
+##  See the GNU Affero General Public License for more details.             ##
+##                                                                          ##
+##  You should have received a copy of the GNU Affero General Public        ##
+##  License along with this program.                                        ##
+##  If not, see <http://www.gnu.org/licenses/>.                             ##
+##                                                                          ##
+##                             Contacts:                                    ##
+##      Nicolas Bellot  : nicolas.bellot@inria.fr                           ##
+##      Adrien Leman    : adrien.leman@inria.fr                             ##
+##      Thomas Gautrais : thomas.gautrais@inria.fr                          ##
+##      Luc Le Magoarou : luc.le-magoarou@inria.fr                          ##
+##      Remi Gribonval  : remi.gribonval@inria.fr                           ##
+##############################################################################
+
+
 set(CTEST_PROJECT_NAME "faust")
 set(CTEST_NIGHTLY_START_TIME "18:30:00 CET")
 set(CTEST_DROP_METHOD "http")
