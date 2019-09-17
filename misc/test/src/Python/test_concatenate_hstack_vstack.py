fa57c3a35d06f163e6cbac3a2dda4a8430328189
hhakim <hakim.hadj-djilani@inria.fr>
2019-09-17 09:21:57 +0200

    Add a test for pyfaust concatenate/hstack/vstack.

---
+++ misc/test/src/Python/test_concatenate_hstack_vstack.py
@@ -0,0 +1,24 @@
+from pyfaust import FaustFactory as FF
+from pyfaust import *
+import numpy as np
+
+F1 = FF.rand(5,50)
+F2 = FF.rand(5,50)
+
+
+concatenate((F1, F2), axis=0)
+concatenate((F1, F2), axis=1)
+
+assert(np.allclose(hstack((F1, F2)).toarray(), concatenate((F1, F2),
+                                                           axis=1).toarray()))
+assert(np.allclose(vstack((F1, F2)).toarray(), concatenate((F1, F2),
+                                                           axis=0).toarray()))
+
+A1 = F1.toarray()
+A2 = F2.toarray()
+assert(np.allclose(np.concatenate((A1, A2), axis=0), concatenate((A1,A2),
+                                                              axis=0)))
+assert(np.allclose(np.vstack((A1, A2)), vstack((A1,A2))))
+assert(np.allclose(np.hstack((A1, A2)), hstack((A1,A2))))
+
+vstack((F1,F2,A2))
