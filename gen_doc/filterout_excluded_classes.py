b92472b5d67477cac94b7a41a4c7521951ed56f9
hhakim <hakim.hadj-djilani@inria.fr>
2020-01-21 17:57:32 +0100

    Filter excluded/non-documented classes from doxydoc (only for pyfaust).

---
+++ gen_doc/filterout_excluded_classes.py
@@ -0,0 +1,30 @@
+import sys
+import re
+
+"""
+This script intends to filter out the doxygen non-documented classes (set in EXCLUDE_SYMBOLS)
+"""
+
+# example to filter
+# <tr class="memitem:"><td class="memItemLeft" align="right" valign="top">class &#160;</td><td class="memItemRight" valign="bottom"><b>ConstraintReal</b></td></tr>                                                 
+# <tr class="memdesc:"><td class="mdescLeft">&#160;</td><td class="mdescRight">This class represents a real constraint on a matrix. <br /></td></tr>                                                                
+# <tr class="separator:"><td class="memSeparator" colspan="2">&#160;</td></tr>
+
+# example to keep
+# <tr class="memitem:"><td class="memItemLeft" align="right" valign="top">class &#160;</td><td class="memItemRight" valign="bottom"><a class="el" href="classmatfaust_1_1factparams_1_1ParamsFact.html">ParamsFact</a></td></tr>
+# <tr class="memdesc:"><td class="mdescLeft">&#160;</td><td class="mdescRight">The parent abstract class to represent the general factorization parameters.  <a href="classmatfaust_1_1factparams_1_1ParamsFact.html#details">More...</a><br /></td></tr>
+# <tr class="separator:"><td class="memSeparator" colspan="2">&#160;</td></tr>
+
+
+in_block_to_del=False
+count_in_block_lines = 0
+for line in sys.stdin:
+    if(in_block_to_del):
+        count_in_block_lines -= 1
+        if(count_in_block_lines <= 0):
+            in_block_to_del = False
+    elif(re.match('.*class &#160.*"bottom"><b>', line)):
+        in_block_to_del=True
+        count_in_block_lines = 2
+    else:
+        print(line, end='')
