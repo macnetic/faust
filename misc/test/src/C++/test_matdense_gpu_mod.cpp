9e237801c7498268c9ee2eeb50ca4243218880cd
hhakim <hakim.hadj-djilani@inria.fr>
2020-09-02 15:43:24 +0200

    Start new MatDense<FPP,GPU2> impl. using GPU_MOD (to replace later MatDense<FPP,GPU>.

---
+++ misc/test/src/C++/test_matdense_gpu_mod.cpp
@@ -0,0 +1,9 @@
+#include "faust_MatDense_gpu2.h"
+#include <cstdlib>
+using namespace Faust;
+
+int main(int argc, char** argv)
+{
+	MatDense<FPP,GPU2> mat;
+	return EXIT_SUCCESS;
+}
---
+++ src/faust_linear_operator/GPU2/faust_MatDense_gpu.h
@@ -0,0 +1,34 @@
+#ifndef __FAUST_MATDENSE_GPU2__
+#define __FAUST_MATDENSE_GPU2__
+#ifdef USE_GPU_MOD
+#include "faust_MatDense.h"
+#define __GM_LOADER__
+#include "gm_interf.h"
+namespace Faust
+{
+
+	template<typename FPP,FDevice DEVICE>
+    class MatDense;
+
+	template<typename FPP>
+		class MatDense<FPP, GPU2> : MatDense<FPP, Cpu>
+		{
+			public:
+				MatDense(const FPP* data, const faust_unsigned_int nbRow, const faust_unsigned_int nbCol);
+
+//				multiply(const MatDense<Cpu,FPP> &other);
+//				multiply(const MatSparse<Cpu,FPP> &other);
+//				multiply(const Vect<Cpu,FPP> &vec);
+//				multiply(const MatDense<GPU2, FPP> &other);
+			private:
+				static void* dsm_funcs;
+				gm_DenseMat_t gpu_mat;
+		};
+
+	template <typename FPP>
+		void* Faust::MatDense<FPP,GPU2>::dsm_funcs = nullptr;
+
+};
+#include "faust_MatDense_gpu_double.hpp"
+#endif
+#endif
