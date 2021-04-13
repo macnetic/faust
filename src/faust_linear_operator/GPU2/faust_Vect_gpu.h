cd61c0db0c3127e1754108f603a0acbda15afc27
hhakim <hakim.hadj-djilani@inria.fr>
2021-04-13 10:15:39 +0200

    Implement c++ Vect<FPP, GPU2>::multiplyLeft(MatSparse<FPP, GPU2>).

--- src/faust_linear_operator/GPU2/faust_Vect_gpu.h
+++ src/faust_linear_operator/GPU2/faust_Vect_gpu.h
@@ -46,7 +46,7 @@ namespace Faust
 			FPP mean_relative_error(const Vect<FPP,GPU2>& ref_vec) const;
 			// delete parent methods that don't apply to a vector
 			void setEyes() = delete;
-			void multiplyLeft(MatSparse<FPP,GPU2> const& S, const char transS);
+			void multiplyLeft(MatSparse<FPP,GPU2> const& S, const char transS='N');
 			FPP operator[](faust_unsigned_int i);
 //			FPP& operator[](faust_unsigned_int i); // impossible to return a reference to a GPU buffer value (therefore it's impossible to modify a vector coeff as it's done with Vect<FPP,Cpu> (e.g. v[i] = 3)
 			void set_coeff(faust_unsigned_int i, const FPP& val);
