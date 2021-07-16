586402f23e8c42ac04d8540f36fc11cae55cad9c
hhakim <hakim.hadj-djilani@inria.fr>
2021-07-16 20:12:17 +0200

    Fix cmake script to add Torch to includes of faust_linear_algebra target + reformat faust_torch.cpp and fix missing namespace.

--- src/faust_linear_operator/CPU/faust_torch.cpp
+++ src/faust_linear_operator/CPU/faust_torch.cpp
@@ -1,70 +1,18 @@
 #include "faust_torch.h"
-
-void display_TensorList(std::vector<torch::Tensor>& tl, const bool transpose /*= true*/)
+namespace Faust
 {
-	if(transpose)
-		for (int i = tl.size()-1; i >= 0; i--) {cout << "Tensor: " << tl.size()-1-i << " [" << tl[i].size(1) << "x" << tl[i].size(0) << "] " << (tl[i].is_sparse()?"SPARSE":"DENSE"); cout << endl;}
-	else
-		for (int i = 0; i < tl.size(); i++) {cout << "Tensor: " << i << " [" << tl[i].size(0) << "x" << tl[i].size(1) << "] " << (tl[i].is_sparse()?"SPARSE":"DENSE"); cout << endl;};
-}
-
-torch::Tensor tensor_chain_mul(const std::vector<torch::Tensor>& tl, const torch::Tensor* op, at::DeviceType dev, const bool chain_opt, const bool contiguous_dense_to_torch, const bool op_on_left /*=true*/)
+	void display_TensorList(std::vector<torch::Tensor>& tl, const bool transpose /*= true*/)
 	{
-		bool all_dense = true;
-		std::vector<torch::Tensor> tlc;
-		for(auto t: tl)
-		{
-			all_dense &= !t.is_sparse();
-			tlc.push_back(t);
-		}
-		if(op)
-		{
-			all_dense &= !op->is_sparse();
-			if(op_on_left)
-				tlc.insert(tlc.begin(), *op);
-			else
-				tlc.push_back(*op);
-		}
-		if(all_dense)
-			return torch::chain_matmul(tlc); //chain_opt is useless because I suppose torch does its own chain opt.
-		if(chain_opt)
-			return std::move(tensor_chain_mul_opt(tlc, nullptr, dev));
-		auto it = tlc.end()-1;
-		auto res = *(it);
-		if(res.is_sparse())
-			res = res.to_dense();
-		std::vector<torch::Tensor> dense_contiguous_facts;
-		while(it != tlc.begin())
-		{
-			auto f = *(--it);
-			if(f.is_sparse())
+		if(transpose)
+			for (int i = tl.size()-1; i >= 0; i--)
 			{
-				if(dense_contiguous_facts.size() > 0)
-				{
-					//multiply chain of dense tensors before
-					dense_contiguous_facts.push_back(res);
-					res = torch::chain_matmul(dense_contiguous_facts);
-					dense_contiguous_facts.erase(dense_contiguous_facts.begin(), dense_contiguous_facts.end());
-				}
-				res = at::_sparse_mm(f, res);
+				cout << "Tensor: " << tl.size()-1-i << " [" << tl[i].size(1) << "x" << tl[i].size(0) << "] " << (tl[i].is_sparse()?"SPARSE":"DENSE"); cout << endl;
 			}
-			else
-				if(contiguous_dense_to_torch)
-					dense_contiguous_facts.insert(dense_contiguous_facts.begin(), f);
-				else
-					res = torch::matmul(f, res);
-		}
-		if(contiguous_dense_to_torch && dense_contiguous_facts.size() > 0)
-		{
-			//multiply chain of dense tensors at the end/start of tlc
-			dense_contiguous_facts.push_back(res);
-			res = torch::chain_matmul(dense_contiguous_facts);
-			dense_contiguous_facts.erase(dense_contiguous_facts.begin(), dense_contiguous_facts.end());
-		}
-		// don't worry assert is enabled only in debug mode (when DEBUG is defined)
-		assert((op != nullptr && op_on_left && res.size(0) == op->size(0)) || ((! op_on_left || op == nullptr) && res.size(0) == tl[0].size(0)) || op == nullptr);
-		assert(((op == nullptr || op_on_left) && res.size(1) == (*(tl.end()-1)).size(1)) || op != nullptr && res.size(1) == op->size(1));
-		return std::move(res); //explicit move but should work auto because Tensor class overrides move operator= and ctor
+		else
+			for (int i = 0; i < tl.size(); i++)
+			{
+				cout << "Tensor: " << i << " [" << tl[i].size(0) << "x" << tl[i].size(1) << "] " << (tl[i].is_sparse()?"SPARSE":"DENSE"); cout << endl;
+			};
 	}
 
 	torch::Tensor tensor_chain_mul_opt(const std::vector<torch::Tensor>& ml, const torch::Tensor* op, at::DeviceType dev, const bool op_on_left /* = true */)
@@ -145,3 +93,64 @@ torch::Tensor tensor_chain_mul(const std::vector<torch::Tensor>& tl, const torch
 			delete res;
 		return std::move(t);
 	}
+
+	torch::Tensor tensor_chain_mul(const std::vector<torch::Tensor>& tl, const torch::Tensor* op, at::DeviceType dev, const bool chain_opt, const bool contiguous_dense_to_torch, const bool op_on_left /*=true*/)
+	{
+		bool all_dense = true;
+		std::vector<torch::Tensor> tlc;
+		for(auto t: tl)
+		{
+			all_dense &= !t.is_sparse();
+			tlc.push_back(t);
+		}
+		if(op)
+		{
+			all_dense &= !op->is_sparse();
+			if(op_on_left)
+				tlc.insert(tlc.begin(), *op);
+			else
+				tlc.push_back(*op);
+		}
+		if(all_dense)
+			return torch::chain_matmul(tlc); //chain_opt is useless because I suppose torch does its own chain opt.
+		if(chain_opt)
+			return std::move(tensor_chain_mul_opt(tlc, nullptr, dev));
+		auto it = tlc.end()-1;
+		auto res = *(it);
+		if(res.is_sparse())
+			res = res.to_dense();
+		std::vector<torch::Tensor> dense_contiguous_facts;
+		while(it != tlc.begin())
+		{
+			auto f = *(--it);
+			if(f.is_sparse())
+			{
+				if(dense_contiguous_facts.size() > 0)
+				{
+					//multiply chain of dense tensors before
+					dense_contiguous_facts.push_back(res);
+					res = torch::chain_matmul(dense_contiguous_facts);
+					dense_contiguous_facts.erase(dense_contiguous_facts.begin(), dense_contiguous_facts.end());
+				}
+				res = at::_sparse_mm(f, res);
+			}
+			else
+				if(contiguous_dense_to_torch)
+					dense_contiguous_facts.insert(dense_contiguous_facts.begin(), f);
+				else
+					res = torch::matmul(f, res);
+		}
+		if(contiguous_dense_to_torch && dense_contiguous_facts.size() > 0)
+		{
+			//multiply chain of dense tensors at the end/start of tlc
+			dense_contiguous_facts.push_back(res);
+			res = torch::chain_matmul(dense_contiguous_facts);
+			dense_contiguous_facts.erase(dense_contiguous_facts.begin(), dense_contiguous_facts.end());
+		}
+		// don't worry assert is enabled only in debug mode (when DEBUG is defined)
+		assert((op != nullptr && op_on_left && res.size(0) == op->size(0)) || ((! op_on_left || op == nullptr) && res.size(0) == tl[0].size(0)) || op == nullptr);
+		assert(((op == nullptr || op_on_left) && res.size(1) == (*(tl.end()-1)).size(1)) || op != nullptr && res.size(1) == op->size(1));
+		return std::move(res); //explicit move but should work auto because Tensor class overrides move operator= and ctor
+	}
+
+}
