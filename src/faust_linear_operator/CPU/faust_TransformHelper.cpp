dd40bbeaa7d5ae5c95ff0dc3b6408a9784ff595b
hhakim <hakim.hadj-djilani@inria.fr>
2021-11-17 14:34:55 +0100

    Fix issue #235. VS linking error: multiple TransformHelper::real symbols.

--- src/faust_linear_operator/CPU/faust_TransformHelper.cpp
+++ src/faust_linear_operator/CPU/faust_TransformHelper.cpp
@@ -2,21 +2,6 @@
 
 namespace Faust
 {
-	//TODO: a generic cpp.in would refactor things
-
-
-	template<>
-		TransformHelper<double, Cpu>* TransformHelper<double, Cpu>::real()
-		{
-			return this->clone();
-		}
-
-	template<>
-		TransformHelper<float, Cpu>* TransformHelper<float, Cpu>::real()
-		{
-			return this->clone();
-		}
-
 	template<>
 		TransformHelper<float, Cpu>* TransformHelper<std::complex<float>, Cpu>::real()
 		{
