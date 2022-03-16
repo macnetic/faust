3e654bb3d738f423b87d2a122c414f1149c0dc35
hhakim <hakim.hadj-djilani@inria.fr>
2022-03-16 13:52:48 +0100

    Move MatSparse<FPP, GPU2> from .cpp.in to hpp file because it's not a template specialization.

--- src/faust_linear_operator/GPU2/faust_MatSparse_gpu.hpp
+++ src/faust_linear_operator/GPU2/faust_MatSparse_gpu.hpp
@@ -1,11 +1,26 @@
-template<typename FPP>
-template<typename FPP2>
-Faust::MatSparse<Real<FPP2>, GPU2> Faust::MatSparse<FPP,GPU2>::to_real() const
-{
-	//TODO: should be done directly in GPU memory in gpu_mod (optimization)
-	MatSparse<FPP, Cpu> cpu_smat;
-	auto ffp2_cpu_smat = cpu_smat.template to_real<Real<FPP2>>();
-	MatSparse<Real<FPP2>, GPU2> ffp2_gpu_smat(ffp2_cpu_smat);
-	return ffp2_gpu_smat;
+namespace Faust {
+	template<typename FPP>
+		MatGeneric<FPP,GPU2>* MatSparse<FPP,GPU2>::Clone(const bool isOptimize /*default value=false*/) const
+		{
+			if (isOptimize)
+			{
+				Faust::MatDense<FPP,GPU2> M((*this));
+				return optimize(M,(*this));
+			} else
+			{
+				return new Faust::MatSparse<FPP,GPU2>((*this));
+			}
+		}
+
+	template<typename FPP>
+		template<typename FPP2>
+		Faust::MatSparse<Real<FPP2>, GPU2> Faust::MatSparse<FPP,GPU2>::to_real() const
+		{
+			//TODO: should be done directly in GPU memory in gpu_mod (optimization)
+			MatSparse<FPP, Cpu> cpu_smat;
+			auto ffp2_cpu_smat = cpu_smat.template to_real<Real<FPP2>>();
+			MatSparse<Real<FPP2>, GPU2> ffp2_gpu_smat(ffp2_cpu_smat);
+			return ffp2_gpu_smat;
+		}
 }
 
