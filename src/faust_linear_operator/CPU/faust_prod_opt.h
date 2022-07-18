9edb74f74babd977f946af72c04f6aa638259de2
hhakim <hakim.hadj-djilani@inria.fr>
2022-07-18 11:41:07 +0200

    Move dynprog_multiply and cost computation functions from faust_prod_opt to the new module faust_prod_opt_gen (intended to be generic, for cpu and gpu).

--- src/faust_linear_operator/CPU/faust_prod_opt.h
+++ src/faust_linear_operator/CPU/faust_prod_opt.h
@@ -140,38 +140,6 @@ namespace Faust
 	template<typename FPP, FDevice DEVICE>
 	MatDense<FPP,DEVICE> multiply_par(const std::vector<MatGeneric<FPP,DEVICE>*>& data, const MatDense<FPP,DEVICE> A, const char opThis);
 
-	/**
-	 * Multipy the matrix chain "factors" using the dynamic programming method to determine the order (parenthesis positions) in the product (reducing the computation cost).
-	 */
-	template<typename FPP>
-		MatDense<FPP, Cpu> dynprog_multiply(std::vector<MatGeneric<FPP, Cpu>*>& factors, const char op='N', const MatGeneric<FPP, Cpu>* A=nullptr);
-
-
-	int cost_bsr_dense(int A_bnnz, int A_bm, int A_bn, int B_nrows, int B_ncols, bool B_transp);
-
-
-	int cost_bsr_sparse(int A_bnnz, int A_bm, int A_bn, bool A_transp, int B_nnz);
-
-
-	int cost_bsr_bsr(int A_bnnz, int A_bm, int A_bn, bool A_transp, int B_nnz);
-
-
-	int cost_sparse_dense(int A_nnz, int B_nrows, int B_ncols, bool B_transp);
-
-
-	int cost_sparse_sparse(int A_nrows, int A_ncols, int A_nnz, bool A_transp, int B_nnz);
-
-
-	int cost_sparse_bsr(int A_nnz, int B_bnnz, int B_bm, int B_bn, bool B_transp);
-
-
-	int cost_dense_dense(int A_nrows, int A_ncols, int B_nrows, int B_ncols, bool B_transp);
-
-
-	int cost_dense_sparse(int A_nrows, int A_ncols, bool A_transp, int B_nnz);
-
-
-	int cost_dense_bsr(int A_nrows, int A_ncols, bool A_transp, int B_bnrows, int B_bncols, int B_bnnz);
 
 }
 #include "faust_prod_opt.hpp"
