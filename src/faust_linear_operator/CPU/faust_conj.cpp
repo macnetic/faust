9c8752a12c3684a245e0effd77e7239b700ca6d4
hhakim <hakim.hadj-djilani@inria.fr>
2022-12-14 16:50:21 +0100

    Add a generic conj function that returns the same type as in input contrary to std::conj.

---
+++ src/faust_linear_operator/CPU/faust_conj.cpp
@@ -0,0 +1,29 @@
+#include "faust_conj.h"
+#include <complex>
+
+namespace Faust
+{
+	template<>
+		double conj<double>(const double& e)
+		{
+			return std::conj(e).real();
+		}
+
+	template<>
+		float conj<float>(const float& e)
+		{
+			return std::conj(e).real();
+		}
+
+	template<>
+		std::complex<double> conj<std::complex<double>>(const std::complex<double>& e)
+		{
+			return std::conj(e);
+		}
+
+	template<>
+		std::complex<float> conj<std::complex<float>>(const std::complex<float>& e)
+		{
+			return std::conj(e);
+		}
+}
---
+++ src/faust_linear_operator/CPU/faust_conj.h
@@ -0,0 +1,10 @@
+#include "faust_constant.h"
+namespace Faust {
+
+	template<typename FPP>
+		void conjugate(FPP* elts, faust_unsigned_int n);
+
+	template<typename FPP>
+		FPP conj(const FPP & e);
+
+};
