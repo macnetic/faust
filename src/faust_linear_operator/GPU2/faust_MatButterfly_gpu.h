3f1dcf52a7ab55c584e25c66f1273fbd762cf1e7
hhakim <hakim.hadj-djilani@inria.fr>
2023-01-11 10:57:41 +0100

    Add GPU2 MatButterfly getNbRow/Col/getType and unit tests.

--- src/faust_linear_operator/GPU2/faust_MatButterfly_gpu.h
+++ src/faust_linear_operator/GPU2/faust_MatButterfly_gpu.h
@@ -23,27 +23,27 @@ namespace Faust
 			MatButterfly(const MatSparse<FPP, Cpu> &factor, int level);
 			void setZeros();
 			size_t getNBytes() const;
-				 /*MatType getType() const;
-				 int32_t getNbRow() const;
-				 int32_t getNbCol() const;
-				 MatGeneric<FPP,GPU2>* clone(const int32_t dev_id=-1, const void* stream=nullptr) const;
-				 MatGeneric<FPP,GPU2>* Clone(const bool isOptimize=false) const;
-				 void* get_gpu_mat_ptr() const;
-				 faust_unsigned_int getNonZeros() const;
-				 void transpose();
-				 void conjugate();
-				 void adjoint();
-				//! \brief Returns a sub-group of rows of this matrix as the same type of matrix
-				 MatGeneric<FPP,GPU2>* get_rows(faust_unsigned_int row_id_start, faust_unsigned_int num_rows) const;
-				//! \brief Returns a sub-group of rows of this matrix as the same type of matrix
-				 MatGeneric<FPP,GPU2>* get_rows(faust_unsigned_int* row_ids, faust_unsigned_int num_rows) const;
-				//! \brief Returns a sub-group of columns of this matrix as the same type of matrix
-				 Faust::MatGeneric<FPP,GPU2>* get_cols(faust_unsigned_int col_id_start, faust_unsigned_int num_cols) const;
-				//! \brief Returns a sub-group of columns of this matrix as the same type of matrix
-				 Faust::MatGeneric<FPP,GPU2>* get_cols(faust_unsigned_int* col_ids, faust_unsigned_int num_cols) const;
+			MatType getType() const {return Butterfly;} //TODO: move def in hpp
+			int32_t getNbRow() const {return d1.size();} //TODO: move def in hpp
+			int32_t getNbCol() const {return d1.size();} //TODO: move def in hpp
+			/*MatGeneric<FPP,GPU2>* clone(const int32_t dev_id=-1, const void* stream=nullptr) const;
+			  MatGeneric<FPP,GPU2>* Clone(const bool isOptimize=false) const;
+			  void* get_gpu_mat_ptr() const;
+			  faust_unsigned_int getNonZeros() const;
+			  void transpose();
+			  void conjugate();
+			  void adjoint();
+			//! \brief Returns a sub-group of rows of this matrix as the same type of matrix
+			MatGeneric<FPP,GPU2>* get_rows(faust_unsigned_int row_id_start, faust_unsigned_int num_rows) const;
+			//! \brief Returns a sub-group of rows of this matrix as the same type of matrix
+			MatGeneric<FPP,GPU2>* get_rows(faust_unsigned_int* row_ids, faust_unsigned_int num_rows) const;
+			//! \brief Returns a sub-group of columns of this matrix as the same type of matrix
+			Faust::MatGeneric<FPP,GPU2>* get_cols(faust_unsigned_int col_id_start, faust_unsigned_int num_cols) const;
+			//! \brief Returns a sub-group of columns of this matrix as the same type of matrix
+			Faust::MatGeneric<FPP,GPU2>* get_cols(faust_unsigned_int* col_ids, faust_unsigned_int num_cols) const;
 
-				 void Display() const;
-				 Real<FPP> norm() const;*/
+			void Display() const;
+			Real<FPP> norm() const;*/
 			void multiply(MatDense<FPP, GPU2> &other, const char op_this);
 		};
 
