43b0df43924eb408bafbbcc70619099f952017e6
hhakim <hakim.hadj-djilani@inria.fr>
2023-01-19 16:38:06 +0100

    Remove TransformHelperButterfly<FPP, GPU2> use in cpu to gpu Faust cloning of python wrapper cpp code.

--- wrapper/python/src/FaustCoreCppGPU.hpp
+++ wrapper/python/src/FaustCoreCppGPU.hpp
@@ -13,13 +13,8 @@ FaustCoreCpp<FPP, Cpu>* clone_gpu2cpu(FaustCoreCpp<FPP, GPU2>* gpu_t)
 template<typename FPP>
 FaustCoreCpp<FPP, GPU2>* clone_cpu2gpu(FaustCoreCpp<FPP, Cpu>* cpu_t)
 {
-	Faust::TransformHelper<FPP, GPU2> *th;
-	if(cpu_t->transform->containsOnlyButterflyPerm())
-		th = new Faust::TransformHelperButterfly<FPP, GPU2>(*cpu_t->transform);
-	else
-		th = new Faust::TransformHelper<FPP,GPU2>(*cpu_t->transform);
-
-    FaustCoreCpp<FPP, GPU2>* core = new FaustCoreCpp<FPP, GPU2>(th);
-    return core;
+	auto th = new Faust::TransformHelper<FPP,GPU2>(*cpu_t->transform);
+	FaustCoreCpp<FPP, GPU2>* core = new FaustCoreCpp<FPP, GPU2>(th);
+	return core;
 }
 #endif
