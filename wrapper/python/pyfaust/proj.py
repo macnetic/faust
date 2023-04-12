b3fc0b7f1b23838d8f6428bee63ef48938fabe5a
hhakim <hakim.hadj-djilani@inria.fr>
2023-04-12 10:09:06 +0200

    Fix format of brief line of pyfaust.proj.spcol API doc.

--- wrapper/python/pyfaust/proj.py
+++ wrapper/python/pyfaust/proj.py
@@ -282,7 +282,9 @@ class splin(proj_gen):
 
 class spcol(proj_gen):
     """
-    Functor for the SPCOL projector. A, the image matrix, is defined by \f$
+    Functor for the SPCOL projector.
+
+    A, the image matrix, is defined by \f$
     \forall j \in \{0,...,shape[1]-1\} \f$ the j-th column \f$ A_{*,j}\f$ is
     such that \f$ \| A_{*,j}\|_0 = k,  \| A\|_F = 1\f$ (if normalized == True)
 
