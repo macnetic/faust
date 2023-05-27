0fc807ec4c2abf8c72a39de390b1e1daa21c92c1
hhakim <hakim.hadj-djilani@inria.fr>
2023-05-27 16:09:04 +0200

    Reduce make/bdist_wheel cmd output log to error/building/linking message in pkg_purepy, pkg_linux, pkg_linux_release, pkg_linux_release_static ci jobs.
    It avoids by the way to overpass the job log size limit on gitlab.

--- misc/continuous_integration/jobs/linux/build_rpm_deb_pkgs.sh
+++ misc/continuous_integration/jobs/linux/build_rpm_deb_pkgs.sh
@@ -17,8 +17,8 @@ make 2>&1 | tee /tmp/log_$(basename $0)_make_$(date +%s) | grep "error:"
 cmake -DCMAKE_CXX_COMPILER=clang++ .. # it needs to be made separately because it cleans up other variables (for building both faust.a and python wrapper with clang -- necessary to avoid unresolved c++ symbols which happens by mixing up gcc and clang objects)
 cmake -DBUILD_WRAPPER_PYTHON=ON -DBUILD_WRAPPER_MATLAB=ON -DBUILD_DOCUMENTATION=ON -DCMAKE_INSTALL_PREFIX=/opt/local/faust -DCPACK_PACKAGE_FILE_NAME=faust-$FAUST_VERSION -DCPACK_PACKAGE_VERSION=$FAUST_VERSION -DBUILD_TESTING=OFF -DREMOTE_DATA_URL="$DURL" -DREMOTE_DATA_FILE="$DFILE" -DEXPERIMENTAL_PKG=$EXPERIMENTAL_PKG -DNOPY2=ON -DUSE_GPU_MOD=ON -DCMAKE_PREFIX_PATH=$PWD/../gpu_mod -DBUILD_FLOAT_PYX=ON  -DBUILD_FLOAT_MEX=ON ..
 make clean
-make faust
-make faust_python
+make faust 2>&1 | tee /tmp/log_$(basename $0)_make_faust_$(date +%s) | grep -i 'error:\|Building\|Link\|creating'
+make faust_python 2>&1 | tee /tmp/log_$(basename $0)_make_faust_python_$(date +%s) | grep -i 'error:\|Building\|Link\|creating'
 # generate package via cpack
 cpack -G RPM -C CPackConfig.cmake
 # remove package temporary files
