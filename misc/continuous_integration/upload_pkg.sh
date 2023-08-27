70a95dde5db098a1dc48a6779e9b7c6c9ed866a4
hhakim <hakim.hadj-djilani@inria.fr>
2023-08-27 11:23:21 +0200

    Minor change.
    Code doc of deprecated script.

--- misc/continuous_integration/upload_pkg.sh
+++ misc/continuous_integration/upload_pkg.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+# DEPRECATION: this script was intended to upload FAµST packages on another
+# gitlab project but using package registry is a way better
+
 if [[ -z "$UP_TOKEN" ]]
 then
 	echo UP_TOKEN variable is not set in the environment
 	exit 1
 fi
 
-# at each new tag the branch packages (of faust-packages project) is deleted and created again to avoid storing too many packages
+# on each new tag the branch packages (of faust-packages project) is deleted and created again to avoid storing too many packages
+# UP_TOKEN must be configured in CI/CD variables or as environment variable
 curl --request DELETE --header "PRIVATE-TOKEN: $UP_TOKEN"  "https://gitlab.inria.fr/api/v4/projects/29950/repository/branches/packages"
 curl --request POST --header "PRIVATE-TOKEN: $UP_TOKEN" "https://gitlab.inria.fr/api/v4/projects/29950/repository/branches?branch=packages&ref=master"
 
 for PKG in build/faust-${CI_COMMIT_TAG}.pkg build/faust-$CI_COMMIT_TAG-amd64.exe build/faust-$CI_COMMIT_TAG-x86_64.*
-do 
+do
 	echo
 	curl --request POST --form "branch=packages"  --form "commit_message=Add package $(basename $PKG)"  --form "start_branch=packages"  --form "actions[][action]=create"  --form "actions[][file_path]=$(basename $PKG)"  --form "actions[][content]=@$PKG"  --header "PRIVATE-TOKEN: $UP_TOKEN" "https://gitlab.inria.fr/api/v4/projects/29950/repository/commits" || break
 done
-
